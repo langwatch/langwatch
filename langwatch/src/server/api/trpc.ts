a05c3d6b055c7e45b87f4693eea10036e7437fc0
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2023-10-21 09:53:54 +0200

    First setup of the collector endpoint and vitest

--- langwatch/src/server/api/trpc.ts
+++ langwatch/src/server/api/trpc.ts
@@ -38,7 +38,7 @@ interface CreateContextOptions {
  *
  * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
  */
-const createInnerTRPCContext = (opts: CreateContextOptions) => {
+export const createInnerTRPCContext = (opts: CreateContextOptions) => {
   return {
     session: opts.session,
     prisma,
