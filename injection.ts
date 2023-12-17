5785933362cbef9f4346ca76b4092de4826ca1ae
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2023-12-17 09:55:20 +0100

    First commit

---
+++ injection.ts
@@ -0,0 +1,7 @@
+import type { Dependencies } from "./langwatch/langwatch/src/injection";
+
+const dependencies : Dependencies = {
+  example: "Foo",
+};
+
+export default dependencies;
---
+++ server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+process.env.NEXTJS_DIST_DIR = ".next-sass";
+process.env.DEPENDENCY_INJECTION = `${__dirname}/injection.ts`;
+
+const { startApp } = require("./langwatch/langwatch/src/start");
+
+startApp("./langwatch/langwatch");
