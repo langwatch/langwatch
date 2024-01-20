ad665d745c49bfa22a975553074e7c342f2e92c7
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-01-20 18:27:51 -0300

    Fix small layout issue when preconditions/custom conditions is bigger than 50% of the screen, this doesn't seem to cause any issue

--- langwatch/src/components/HorizontalFormControl.tsx
+++ langwatch/src/components/HorizontalFormControl.tsx
@@ -30,7 +30,7 @@ export function HorizontalFormControl({
       _last={{ border: "none" }}
     >
       <HStack width="full" flexDirection={["column", "column", "row"]} align={align}>
-        <VStack align="start" spacing={1}>
+        <VStack align="start" spacing={1} width="full">
           <FormLabel margin={0}>{label}</FormLabel>
           <FormHelperText margin={0} fontSize={12}>
             {helper}
