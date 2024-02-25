541194e4ad9249b65fd4f2002de893ac6dca987c
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-02-25 09:53:03 +0100

    When formatting ms, round the ms because it can be a float of average milliseconds for example, and show 1 decimal point for minutes because it's still relevant

--- langwatch/src/utils/formatMilliseconds.ts
+++ langwatch/src/utils/formatMilliseconds.ts
@@ -1,7 +1,7 @@
 export const formatMilliseconds = (ms: number): string => {
-  if (ms < 1000) return `${ms}ms`;
+  if (ms < 1000) return `${Math.round(ms)}ms`;
   if (ms >= 1000 && ms < 10000) return `${(ms / 1000).toFixed(1)}s`;
   if (ms >= 10000 && ms < 60000) return `${Math.round(ms / 1000)}s`;
-  if (ms >= 60000 && ms < 3600000) return `${Math.round(ms / 60000)}min`;
+  if (ms >= 60000 && ms < 3600000) return `${(ms / 60000).toFixed(1)}min`;
   return `${(ms / 3600000).toFixed(1)}h`;
 };
