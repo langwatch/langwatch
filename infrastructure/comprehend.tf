47da714c31dcf56da4a001e5c6e037f00b02982c
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-03-21 13:46:18 +0100

    Bump to add AWS comprehend for PII detection

---
+++ infrastructure/comprehend.tf
@@ -0,0 +1,40 @@
+resource "aws_iam_user" "comprehend_user" {
+  name = "comprehend-user"
+}
+
+resource "aws_iam_policy" "comprehend_policy" {
+  name        = "ComprehendDetectPIIEntitiesPolicy"
+  description = "Policy for Comprehend detect_pii_entities action"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "comprehend:DetectPiiEntities"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "comprehend_user_policy_attachment" {
+  user       = aws_iam_user.comprehend_user.name
+  policy_arn = aws_iam_policy.comprehend_policy.arn
+}
+
+resource "aws_iam_access_key" "comprehend_user_key" {
+  user = aws_iam_user.comprehend_user.name
+}
+
+output "aws_access_key_id" {
+  value     = aws_iam_access_key.comprehend_user_key.id
+  sensitive = true
+}
+
+output "aws_secret_access_key" {
+  value     = aws_iam_access_key.comprehend_user_key.secret
+  sensitive = true
+}
