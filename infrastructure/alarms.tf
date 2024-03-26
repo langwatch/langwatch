947e99769e416f3afadb0020575ae3fc10811997
msnmz <snmez.muharrem@gmail.com>
2024-03-26 15:10:40 +0100

    enable intrusion detection system notifications

--- infrastructure/alarms.tf
+++ infrastructure/alarms.tf
@@ -69,3 +69,32 @@ resource "aws_cloudwatch_metric_alarm" "server_errors" {
 
   treat_missing_data = "notBreaching"
 }
+
+resource "aws_cloudwatch_event_rule" "guardduty_findings" {
+  name        = "guardduty-medium-high-findings"
+  description = "Capture medium to high severity GuardDuty findings"
+
+  # severity levels
+  #  Low: < 4.0
+  #  Medium: >= 4.0 and < 7.0
+  #  High: >= 7.0
+  event_pattern = jsonencode({
+    source      = ["aws.guardduty"],
+    detail_type = ["GuardDuty Finding"],
+    detail = {
+      severity = [
+        4, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9,
+        5, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9,
+        6, 6.0, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9,
+        7, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9,
+        8, 8.0, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9
+      ]
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "guardduty_to_sns" {
+  rule      = aws_cloudwatch_event_rule.guardduty_findings.name
+  target_id = "SendToSNS"
+  arn       = aws_sns_topic.alarms.arn
+}
