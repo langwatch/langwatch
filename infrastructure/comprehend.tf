bbb697582b2ed2ca344b7458639bd433a11d9328
msnmz <snmez.muharrem@gmail.com>
2024-03-26 13:30:17 +0100

    replace comprehend user with comprehend role

--- infrastructure/comprehend.tf
+++ infrastructure/comprehend.tf
@@ -1,8 +1,4 @@
-resource "aws_iam_user" "comprehend_user" {
-  name = "comprehend-user"
-}
-
-resource "aws_iam_policy" "comprehend_policy" {
+resource "aws_iam_policy" "comprehend" {
   name        = "ComprehendDetectPIIEntitiesPolicy"
   description = "Policy for Comprehend detect_pii_entities action"
 
@@ -21,21 +17,25 @@ resource "aws_iam_policy" "comprehend_policy" {
   })
 }
 
-resource "aws_iam_user_policy_attachment" "comprehend_user_policy_attachment" {
-  user       = aws_iam_user.comprehend_user.name
-  policy_arn = aws_iam_policy.comprehend_policy.arn
+resource "aws_iam_role" "comprehend" {
+  name = "comprehend-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
 }
 
-resource "aws_iam_access_key" "comprehend_user_key" {
-  user = aws_iam_user.comprehend_user.name
-}
-
-output "aws_comprehend_access_key_id" {
-  value     = aws_iam_access_key.comprehend_user_key.id
-  sensitive = true
-}
-
-output "aws_comprehend_secret_access_key" {
-  value     = aws_iam_access_key.comprehend_user_key.secret
-  sensitive = true
+resource "aws_iam_role_policy_attachment" "comprehend" {
+  role       = aws_iam_role.comprehend.name
+  policy_arn = aws_iam_policy.comprehend.arn
 }
