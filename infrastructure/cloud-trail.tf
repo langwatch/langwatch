52df00a2c6c3d439f8a784c32c7ab5a43a5152d3
msnmz <snmez.muharrem@gmail.com>
2024-03-26 23:46:19 +0100

    enable log file validation for cloud trail

--- infrastructure/cloud-trail.tf
+++ infrastructure/cloud-trail.tf
@@ -41,6 +41,7 @@ resource "aws_cloudtrail" "logs" {
   s3_key_prefix                 = local.cloudtrail_bucket_key_prefix
   include_global_service_events = true
   is_multi_region_trail         = true
+  enable_log_file_validation    = true
 
   event_selector {
     read_write_type           = "All"
