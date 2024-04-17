0e401827ef384483f75e608b17188e3b879148cf
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-04-17 13:17:07 +0200

    Use only langwatch_nlp files as hash for deployment to avoid redeploying every time

--- infrastructure/scripts/get_langwatch_nlp_git_sha.sh
+++ infrastructure/scripts/get_langwatch_nlp_git_sha.sh
@@ -4,10 +4,5 @@ set -e
 
 cd ../langwatch
 
-if test -z "$(git status --porcelain)"; then
-  echo "{\"tag\": \"$(git rev-parse --short HEAD)\"}"
-else
-  # Get the hash of all tracked files, modifications, and untracked files (excluding .git and ignored files)
-  current_hash=$(git ls-files -o -c -m --exclude-standard | grep -v langwatch_nlp | xargs cat | sha256sum | cut -d' ' -f1 | cut -c 1-7)
-  echo "{\"tag\": \"$(git rev-parse --short HEAD)-dirty-${current_hash}\"}"
-fi
+current_hash=$(git ls-files -o -c -m --exclude-standard | grep -v langwatch_nlp | xargs cat | sha256sum | cut -d' ' -f1 | cut -c 1-7)
+echo "{\"tag\": \"${current_hash}\"}"
