1917b48a4f134d8b2054bec918c4fd385d261969
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-04-26 00:36:17 +0200

    Track users actually logging in to langwatch to use it

--- src/components/ExtraFooterComponents.tsx
+++ src/components/ExtraFooterComponents.tsx
@@ -1,6 +1,27 @@
 import Script from "next/script";
+import { useEffect } from "react";
+import { useOrganizationTeamProject } from "../../langwatch/langwatch/src/hooks/useOrganizationTeamProject";
+import { useRequiredSession } from "../../langwatch/langwatch/src/hooks/useRequiredSession";
 
 export function ExtraFooterComponents() {
+  const session = useRequiredSession();
+  const { organization, project } = useOrganizationTeamProject();
+
+  useEffect(() => {
+    const gtag = (window as any).gtag;
+    if (!session.data?.user || !organization || !project || !gtag) return;
+
+    gtag("event", "open_dashboard", {
+      organization_id: organization.id,
+      organization_name: organization.name,
+      project_id: project.id,
+      project_name: project.name,
+      environment: process.env.NODE_ENV,
+      user_name: session.data.user.name,
+      user_id: session.data.user.id,
+    });
+  }, [organization?.id, project?.id]);
+
   return (
     <>
       <Script
