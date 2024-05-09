d795b5b7d2da0f8fb675fd2469e2b358e8ee8302
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-05-09 19:12:10 +0200

    Allow to disable proxy and get running loop so it can work when inside lambdas

--- langwatch_nlp/langwatch_nlp/main.py
+++ langwatch_nlp/langwatch_nlp/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from dotenv import load_dotenv
 from mangum import Mangum
 
@@ -21,12 +22,19 @@ batch_clustering.setup_endpoints(app)
 incremental_clustering.setup_endpoints(app)
 sentiment_analysis.setup_endpoints(app)
 
-proxy_config = litellm_proxy_server.ProxyConfig()
-generate_proxy_config()
-litellm_proxy_server.save_worker_config(config="proxy_config.generated.yaml")
+async def proxy_startup():
+    litellm_proxy_server.ProxyConfig()
+    generate_proxy_config()
+    litellm_proxy_server.save_worker_config(config="proxy_config.generated.yaml")
+    app.mount("/proxy", litellm_proxy_server.app)
+    await litellm_proxy_server.startup_event()
 
-app.mount("/proxy", litellm_proxy_server.app)
-asyncio.run(litellm_proxy_server.startup_event())
+if os.getenv("PROXY_IS_ENABLED") == "1":
+    loop = asyncio.get_event_loop()
+    if not loop.is_running():
+        loop.run_until_complete(proxy_startup())
+    else:
+        asyncio.ensure_future(proxy_startup())
 
 if __name__ != "__main__":
     handler = Mangum(app, lifespan="off")
