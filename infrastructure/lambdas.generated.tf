c86bd504f9e051723aeab99f895c049a225ecfb0
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-05-09 19:32:37 +0200

    Regenerate lambdas file

--- infrastructure/lambdas.generated.tf
+++ infrastructure/lambdas.generated.tf
@@ -1,4 +1,68 @@
 
+resource "aws_api_gateway_resource" "comprehend_pii_detection" {
+    rest_api_id = aws_api_gateway_rest_api.this.id
+    parent_id   = aws_api_gateway_resource.aws.id
+    path_part   = "comprehend_pii_detection"
+}
+
+module "aws-comprehend_pii_detection-api-gw" {
+    source                 = "./api-gw-resource"
+    apigw_id               = aws_api_gateway_rest_api.this.id
+    apigw_root_resource_id = aws_api_gateway_resource.comprehend_pii_detection.id
+    path                   = "evaluate"
+    method                 = "POST"
+
+    lambda_invoke_arn = module.aws-evaluator.lambda_invoke_arn
+}
+
+module "aws-evaluator" {
+    source              = "./lambda"
+    evaluator_package   = "aws"
+    sns_alarms_topic_arn = aws_sns_topic.alarms.arn
+    apigw_execution_arn = aws_api_gateway_rest_api.this.execution_arn
+    environment_variables = {
+        AWS_COMPREHEND_ACCESS_KEY_ID = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["AWS_COMPREHEND_ACCESS_KEY_ID"]
+
+        AWS_COMPREHEND_SECRET_ACCESS_KEY = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["AWS_COMPREHEND_SECRET_ACCESS_KEY"]
+    }
+}
+
+resource "aws_api_gateway_resource" "aws" {
+    rest_api_id = aws_api_gateway_rest_api.this.id
+    parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+    path_part   = "aws"
+}
+
+resource "aws_api_gateway_resource" "language_detection" {
+    rest_api_id = aws_api_gateway_rest_api.this.id
+    parent_id   = aws_api_gateway_resource.lingua.id
+    path_part   = "language_detection"
+}
+
+module "lingua-language_detection-api-gw" {
+    source                 = "./api-gw-resource"
+    apigw_id               = aws_api_gateway_rest_api.this.id
+    apigw_root_resource_id = aws_api_gateway_resource.language_detection.id
+    path                   = "evaluate"
+    method                 = "POST"
+
+    lambda_invoke_arn = module.lingua-evaluator.lambda_invoke_arn
+}
+
+module "lingua-evaluator" {
+    source              = "./lambda"
+    evaluator_package   = "lingua"
+    sns_alarms_topic_arn = aws_sns_topic.alarms.arn
+    apigw_execution_arn = aws_api_gateway_rest_api.this.execution_arn
+    environment_variables = {}
+}
+
+resource "aws_api_gateway_resource" "lingua" {
+    rest_api_id = aws_api_gateway_rest_api.this.id
+    parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+    path_part   = "lingua"
+}
+
 resource "aws_api_gateway_resource" "content_safety" {
     rest_api_id = aws_api_gateway_rest_api.this.id
     parent_id   = aws_api_gateway_resource.azure.id
@@ -47,38 +111,6 @@ resource "aws_api_gateway_resource" "azure" {
     rest_api_id = aws_api_gateway_rest_api.this.id
     parent_id   = aws_api_gateway_rest_api.this.root_resource_id
     path_part   = "azure"
-}
-
-resource "aws_api_gateway_resource" "moderation" {
-    rest_api_id = aws_api_gateway_rest_api.this.id
-    parent_id   = aws_api_gateway_resource.openai.id
-    path_part   = "moderation"
-}
-
-module "openai-moderation-api-gw" {
-    source                 = "./api-gw-resource"
-    apigw_id               = aws_api_gateway_rest_api.this.id
-    apigw_root_resource_id = aws_api_gateway_resource.moderation.id
-    path                   = "evaluate"
-    method                 = "POST"
-
-    lambda_invoke_arn = module.openai-evaluator.lambda_invoke_arn
-}
-
-module "openai-evaluator" {
-    source              = "./lambda"
-    evaluator_package   = "openai"
-    sns_alarms_topic_arn = aws_sns_topic.alarms.arn
-    apigw_execution_arn = aws_api_gateway_rest_api.this.execution_arn
-    environment_variables = {
-        OPENAI_API_KEY = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["OPENAI_API_KEY"]
-    }
-}
-
-resource "aws_api_gateway_resource" "openai" {
-    rest_api_id = aws_api_gateway_rest_api.this.id
-    parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-    path_part   = "openai"
 }
 
 resource "aws_api_gateway_resource" "answer_relevancy" {
@@ -185,9 +217,9 @@ module "ragas-evaluator" {
     environment_variables = {
         AZURE_API_KEY = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["AZURE_API_KEY"]
 
-        AZURE_API_BASE = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["AZURE_API_BASE"]
-
         OPENAI_API_KEY = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["OPENAI_API_KEY"]
+
+        AZURE_API_BASE = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["AZURE_API_BASE"]
     }
 }
 
@@ -199,150 +231,18 @@ resource "aws_api_gateway_resource" "ragas" {
 
 resource "aws_api_gateway_resource" "basic" {
     rest_api_id = aws_api_gateway_rest_api.this.id
-    parent_id   = aws_api_gateway_resource.custom.id
+    parent_id   = aws_api_gateway_resource.langevals.id
     path_part   = "basic"
 }
 
-module "custom-basic-api-gw" {
+module "langevals-basic-api-gw" {
     source                 = "./api-gw-resource"
     apigw_id               = aws_api_gateway_rest_api.this.id
     apigw_root_resource_id = aws_api_gateway_resource.basic.id
     path                   = "evaluate"
     method                 = "POST"
 
-    lambda_invoke_arn = module.custom-evaluator.lambda_invoke_arn
-}
-
-resource "aws_api_gateway_resource" "llm_boolean" {
-    rest_api_id = aws_api_gateway_rest_api.this.id
-    parent_id   = aws_api_gateway_resource.custom.id
-    path_part   = "llm_boolean"
-}
-
-module "custom-llm_boolean-api-gw" {
-    source                 = "./api-gw-resource"
-    apigw_id               = aws_api_gateway_rest_api.this.id
-    apigw_root_resource_id = aws_api_gateway_resource.llm_boolean.id
-    path                   = "evaluate"
-    method                 = "POST"
-
-    lambda_invoke_arn = module.custom-evaluator.lambda_invoke_arn
-}
-
-resource "aws_api_gateway_resource" "llm_score" {
-    rest_api_id = aws_api_gateway_rest_api.this.id
-    parent_id   = aws_api_gateway_resource.custom.id
-    path_part   = "llm_score"
-}
-
-module "custom-llm_score-api-gw" {
-    source                 = "./api-gw-resource"
-    apigw_id               = aws_api_gateway_rest_api.this.id
-    apigw_root_resource_id = aws_api_gateway_resource.llm_score.id
-    path                   = "evaluate"
-    method                 = "POST"
-
-    lambda_invoke_arn = module.custom-evaluator.lambda_invoke_arn
-}
-
-resource "aws_api_gateway_resource" "similarity" {
-    rest_api_id = aws_api_gateway_rest_api.this.id
-    parent_id   = aws_api_gateway_resource.custom.id
-    path_part   = "similarity"
-}
-
-module "custom-similarity-api-gw" {
-    source                 = "./api-gw-resource"
-    apigw_id               = aws_api_gateway_rest_api.this.id
-    apigw_root_resource_id = aws_api_gateway_resource.similarity.id
-    path                   = "evaluate"
-    method                 = "POST"
-
-    lambda_invoke_arn = module.custom-evaluator.lambda_invoke_arn
-}
-
-module "custom-evaluator" {
-    source              = "./lambda"
-    evaluator_package   = "custom"
-    sns_alarms_topic_arn = aws_sns_topic.alarms.arn
-    apigw_execution_arn = aws_api_gateway_rest_api.this.execution_arn
-    environment_variables = {
-        AZURE_API_KEY = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["AZURE_API_KEY"]
-
-        AZURE_API_BASE = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["AZURE_API_BASE"]
-
-        OPENAI_API_KEY = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["OPENAI_API_KEY"]
-    }
-}
-
-resource "aws_api_gateway_resource" "custom" {
-    rest_api_id = aws_api_gateway_rest_api.this.id
-    parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-    path_part   = "custom"
-}
-
-resource "aws_api_gateway_resource" "language_detection" {
-    rest_api_id = aws_api_gateway_rest_api.this.id
-    parent_id   = aws_api_gateway_resource.lingua.id
-    path_part   = "language_detection"
-}
-
-module "lingua-language_detection-api-gw" {
-    source                 = "./api-gw-resource"
-    apigw_id               = aws_api_gateway_rest_api.this.id
-    apigw_root_resource_id = aws_api_gateway_resource.language_detection.id
-    path                   = "evaluate"
-    method                 = "POST"
-
-    lambda_invoke_arn = module.lingua-evaluator.lambda_invoke_arn
-}
-
-module "lingua-evaluator" {
-    source              = "./lambda"
-    evaluator_package   = "lingua"
-    sns_alarms_topic_arn = aws_sns_topic.alarms.arn
-    apigw_execution_arn = aws_api_gateway_rest_api.this.execution_arn
-    environment_variables = {}
-}
-
-resource "aws_api_gateway_resource" "lingua" {
-    rest_api_id = aws_api_gateway_rest_api.this.id
-    parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-    path_part   = "lingua"
-}
-
-resource "aws_api_gateway_resource" "comprehend_pii_detection" {
-    rest_api_id = aws_api_gateway_rest_api.this.id
-    parent_id   = aws_api_gateway_resource.aws.id
-    path_part   = "comprehend_pii_detection"
-}
-
-module "aws-comprehend_pii_detection-api-gw" {
-    source                 = "./api-gw-resource"
-    apigw_id               = aws_api_gateway_rest_api.this.id
-    apigw_root_resource_id = aws_api_gateway_resource.comprehend_pii_detection.id
-    path                   = "evaluate"
-    method                 = "POST"
-
-    lambda_invoke_arn = module.aws-evaluator.lambda_invoke_arn
-}
-
-module "aws-evaluator" {
-    source              = "./lambda"
-    evaluator_package   = "aws"
-    sns_alarms_topic_arn = aws_sns_topic.alarms.arn
-    apigw_execution_arn = aws_api_gateway_rest_api.this.execution_arn
-    environment_variables = {
-        AWS_COMPREHEND_ACCESS_KEY_ID = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["AWS_COMPREHEND_ACCESS_KEY_ID"]
-
-        AWS_COMPREHEND_SECRET_ACCESS_KEY = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["AWS_COMPREHEND_SECRET_ACCESS_KEY"]
-    }
-}
-
-resource "aws_api_gateway_resource" "aws" {
-    rest_api_id = aws_api_gateway_rest_api.this.id
-    parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-    path_part   = "aws"
+    lambda_invoke_arn = module.langevals-evaluator.lambda_invoke_arn
 }
 
 resource "aws_api_gateway_resource" "competitor_blocklist" {
@@ -377,6 +277,38 @@ module "langevals-competitor_llm-api-gw" {
     lambda_invoke_arn = module.langevals-evaluator.lambda_invoke_arn
 }
 
+resource "aws_api_gateway_resource" "llm_boolean" {
+    rest_api_id = aws_api_gateway_rest_api.this.id
+    parent_id   = aws_api_gateway_resource.langevals.id
+    path_part   = "llm_boolean"
+}
+
+module "langevals-llm_boolean-api-gw" {
+    source                 = "./api-gw-resource"
+    apigw_id               = aws_api_gateway_rest_api.this.id
+    apigw_root_resource_id = aws_api_gateway_resource.llm_boolean.id
+    path                   = "evaluate"
+    method                 = "POST"
+
+    lambda_invoke_arn = module.langevals-evaluator.lambda_invoke_arn
+}
+
+resource "aws_api_gateway_resource" "llm_score" {
+    rest_api_id = aws_api_gateway_rest_api.this.id
+    parent_id   = aws_api_gateway_resource.langevals.id
+    path_part   = "llm_score"
+}
+
+module "langevals-llm_score-api-gw" {
+    source                 = "./api-gw-resource"
+    apigw_id               = aws_api_gateway_rest_api.this.id
+    apigw_root_resource_id = aws_api_gateway_resource.llm_score.id
+    path                   = "evaluate"
+    method                 = "POST"
+
+    lambda_invoke_arn = module.langevals-evaluator.lambda_invoke_arn
+}
+
 resource "aws_api_gateway_resource" "off_topic" {
     rest_api_id = aws_api_gateway_rest_api.this.id
     parent_id   = aws_api_gateway_resource.langevals.id
@@ -393,6 +325,22 @@ module "langevals-off_topic-api-gw" {
     lambda_invoke_arn = module.langevals-evaluator.lambda_invoke_arn
 }
 
+resource "aws_api_gateway_resource" "similarity" {
+    rest_api_id = aws_api_gateway_rest_api.this.id
+    parent_id   = aws_api_gateway_resource.langevals.id
+    path_part   = "similarity"
+}
+
+module "langevals-similarity-api-gw" {
+    source                 = "./api-gw-resource"
+    apigw_id               = aws_api_gateway_rest_api.this.id
+    apigw_root_resource_id = aws_api_gateway_resource.similarity.id
+    path                   = "evaluate"
+    method                 = "POST"
+
+    lambda_invoke_arn = module.langevals-evaluator.lambda_invoke_arn
+}
+
 module "langevals-evaluator" {
     source              = "./lambda"
     evaluator_package   = "langevals"
@@ -401,9 +349,9 @@ module "langevals-evaluator" {
     environment_variables = {
         AZURE_API_KEY = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["AZURE_API_KEY"]
 
-        AZURE_API_BASE = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["AZURE_API_BASE"]
-
         OPENAI_API_KEY = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["OPENAI_API_KEY"]
+
+        AZURE_API_BASE = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["AZURE_API_BASE"]
     }
 }
 
@@ -411,6 +359,38 @@ resource "aws_api_gateway_resource" "langevals" {
     rest_api_id = aws_api_gateway_rest_api.this.id
     parent_id   = aws_api_gateway_rest_api.this.root_resource_id
     path_part   = "langevals"
+}
+
+resource "aws_api_gateway_resource" "moderation" {
+    rest_api_id = aws_api_gateway_rest_api.this.id
+    parent_id   = aws_api_gateway_resource.openai.id
+    path_part   = "moderation"
+}
+
+module "openai-moderation-api-gw" {
+    source                 = "./api-gw-resource"
+    apigw_id               = aws_api_gateway_rest_api.this.id
+    apigw_root_resource_id = aws_api_gateway_resource.moderation.id
+    path                   = "evaluate"
+    method                 = "POST"
+
+    lambda_invoke_arn = module.openai-evaluator.lambda_invoke_arn
+}
+
+module "openai-evaluator" {
+    source              = "./lambda"
+    evaluator_package   = "openai"
+    sns_alarms_topic_arn = aws_sns_topic.alarms.arn
+    apigw_execution_arn = aws_api_gateway_rest_api.this.execution_arn
+    environment_variables = {
+        OPENAI_API_KEY = jsondecode(data.aws_secretsmanager_secret_version.langevals.secret_string)["OPENAI_API_KEY"]
+    }
+}
+
+resource "aws_api_gateway_resource" "openai" {
+    rest_api_id = aws_api_gateway_rest_api.this.id
+    parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+    path_part   = "openai"
 }
 
 resource "aws_api_gateway_resource" "dlp_pii_detection" {
@@ -449,11 +429,11 @@ resource "aws_api_gateway_deployment" "this" {
     count = module.variables.profile == "lw-prod" ? 1 : 0
 
     triggers = {
-        redeployment = sha1(jsonencode([module.azure-content_safety-api-gw, module.azure-jailbreak-api-gw, module.openai-moderation-api-gw, module.ragas-answer_relevancy-api-gw, module.ragas-context_precision-api-gw, module.ragas-context_recall-api-gw, module.ragas-context_relevancy-api-gw, module.ragas-context_utilization-api-gw, module.ragas-faithfulness-api-gw, module.custom-basic-api-gw, module.custom-llm_boolean-api-gw, module.custom-llm_score-api-gw, module.custom-similarity-api-gw, module.lingua-language_detection-api-gw, module.aws-comprehend_pii_detection-api-gw, module.langevals-competitor_blocklist-api-gw, module.langevals-competitor_llm-api-gw, module.langevals-off_topic-api-gw, module.google_cloud-dlp_pii_detection-api-gw]))
+        redeployment = sha1(jsonencode([module.aws-comprehend_pii_detection-api-gw, module.lingua-language_detection-api-gw, module.azure-content_safety-api-gw, module.azure-jailbreak-api-gw, module.ragas-answer_relevancy-api-gw, module.ragas-context_precision-api-gw, module.ragas-context_recall-api-gw, module.ragas-context_relevancy-api-gw, module.ragas-context_utilization-api-gw, module.ragas-faithfulness-api-gw, module.langevals-basic-api-gw, module.langevals-competitor_blocklist-api-gw, module.langevals-competitor_llm-api-gw, module.langevals-llm_boolean-api-gw, module.langevals-llm_score-api-gw, module.langevals-off_topic-api-gw, module.langevals-similarity-api-gw, module.openai-moderation-api-gw, module.google_cloud-dlp_pii_detection-api-gw]))
     }
 
     depends_on = [
-        module.azure-content_safety-api-gw, module.azure-jailbreak-api-gw, module.openai-moderation-api-gw, module.ragas-answer_relevancy-api-gw, module.ragas-context_precision-api-gw, module.ragas-context_recall-api-gw, module.ragas-context_relevancy-api-gw, module.ragas-context_utilization-api-gw, module.ragas-faithfulness-api-gw, module.custom-basic-api-gw, module.custom-llm_boolean-api-gw, module.custom-llm_score-api-gw, module.custom-similarity-api-gw, module.lingua-language_detection-api-gw, module.aws-comprehend_pii_detection-api-gw, module.langevals-competitor_blocklist-api-gw, module.langevals-competitor_llm-api-gw, module.langevals-off_topic-api-gw, module.google_cloud-dlp_pii_detection-api-gw
+        module.aws-comprehend_pii_detection-api-gw, module.lingua-language_detection-api-gw, module.azure-content_safety-api-gw, module.azure-jailbreak-api-gw, module.ragas-answer_relevancy-api-gw, module.ragas-context_precision-api-gw, module.ragas-context_recall-api-gw, module.ragas-context_relevancy-api-gw, module.ragas-context_utilization-api-gw, module.ragas-faithfulness-api-gw, module.langevals-basic-api-gw, module.langevals-competitor_blocklist-api-gw, module.langevals-competitor_llm-api-gw, module.langevals-llm_boolean-api-gw, module.langevals-llm_score-api-gw, module.langevals-off_topic-api-gw, module.langevals-similarity-api-gw, module.openai-moderation-api-gw, module.google_cloud-dlp_pii_detection-api-gw
     ]
 
     rest_api_id = aws_api_gateway_rest_api.this.id
