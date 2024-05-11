52a5b3c5f2778d913adb9262e54b63ba2a2b8c54
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-05-11 21:35:38 +0200

    Just leave it as a TODO

--- infrastructure/scripts/get_langwatch_saas_git_sha.sh
+++ infrastructure/scripts/get_langwatch_saas_git_sha.sh
@@ -6,6 +6,7 @@ cd ..
 
 salt="2024-05-11"
 files=$(git ls-files -o -c -m --exclude-standard | grep -v ^infrastructure | grep -v ^langwatch | grep -v ^src/pages/api | grep -v ^langevals | grep -v ^.github | grep -v ^.gitmodules | xargs cat)
+# TODO: from some reason this hash still mismatches from mac local to the one in the CI
 current_hash=$(printf "%s%s" "$files" "$salt" | sha256sum | cut -d' ' -f1 | cut -c 1-7)
 cd infrastructure
 current_hash_langwatch=$(./scripts/get_langwatch_git_sha.sh | jq -r '.tag')
