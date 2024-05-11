1f853e2c4ba8c897351e2f77c60cbb38db49c394
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-05-11 20:25:16 +0200

    Fix getting hash from langwatch-sass, failing for whatever reason where before it didnt *shrug*

--- infrastructure/scripts/get_langwatch_saas_git_sha.sh
+++ infrastructure/scripts/get_langwatch_saas_git_sha.sh
@@ -5,7 +5,7 @@ set -e
 cd ..
 
 salt="2024-05-11"
-files=$(git ls-files -o -c -m --exclude-standard | grep -v ^infrastructure | grep -v ^langevals | grep -v ^.github | xargs cat)
+files=$(git ls-files -o -c -m --exclude-standard | grep -v ^infrastructure | grep -v ^langwatch | grep -v ^src/pages/api | grep -v ^langevals | grep -v ^.github |  xargs cat)
 current_hash=$(echo -e "$files $salt" | sha256sum | cut -d' ' -f1 | cut -c 1-7)
 cd infrastructure
 current_hash_langwatch=$(./scripts/get_langwatch_git_sha.sh | jq -r '.tag')
