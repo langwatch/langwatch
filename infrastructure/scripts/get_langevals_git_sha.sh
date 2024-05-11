b8a37a1c9d463766154d3a81cec0d16fbc59f471
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-05-11 08:23:47 +0200

    Build only the evaluators lambdas that actually change

--- infrastructure/scripts/get_langevals_git_sha.sh
+++ infrastructure/scripts/get_langevals_git_sha.sh
@@ -4,5 +4,10 @@ set -e
 
 cd ../langevals
 
-current_hash=$(git ls-files -o -c -m --exclude-standard | xargs cat | sha256sum | cut -d' ' -f1 | cut -c 1-7)
+evaluator=$1
+
+global_affecting_files=$(git ls-files -o -c -m --exclude-standard | grep -v ^.github | grep -v ^README.md | grep -v ^evaluators | grep -v ^notebooks | grep -v ^tests | grep -v ^ts-integration)
+evaluators_files=$(git ls-files -o -c -m --exclude-standard | grep ^evaluators/$evaluator)
+all_files="$global_affecting_files\n$evaluators_files"
+current_hash=$(echo -e "$all_files" | xargs cat | sha256sum | cut -d' ' -f1 | cut -c 1-7)
 echo "{\"tag\": \"hash-${current_hash}\", \"git_tag\": \"git-$(git rev-parse --short HEAD)\"}"
