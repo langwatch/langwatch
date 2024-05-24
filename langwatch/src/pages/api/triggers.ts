3b9c362f950bba1fee774a099fd908bc6dfd0cd3
Richard Huth <richhuth@gmail.com>
2024-05-24 13:42:10 +0200

    Update api method to GET

--- langwatch/src/pages/api/triggers.ts
+++ langwatch/src/pages/api/triggers.ts
@@ -16,8 +16,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    return res.status(405).end(); // Only accept POST requests
+  if (req.method !== "GET") {
+    return res.status(405).end();
   }
 
   const projects = await prisma.project.findMany({
