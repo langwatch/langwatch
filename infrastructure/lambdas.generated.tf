37773d2952209ca94814c6b358fa83a3069f4562
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-06-05 22:38:13 +0200

    Add sentiment polarity

--- infrastructure/lambdas.generated.tf
+++ infrastructure/lambdas.generated.tf
@@ -307,6 +307,22 @@ module "langevals-off_topic-api-gw" {
     lambda_invoke_arn = module.langevals-evaluator.lambda_invoke_arn
 }
 
+resource "aws_api_gateway_resource" "product_sentiment_polarity" {
+    rest_api_id = aws_api_gateway_rest_api.langevals.id
+    parent_id   = aws_api_gateway_resource.langevals.id
+    path_part   = "product_sentiment_polarity"
+}
+
+module "langevals-product_sentiment_polarity-api-gw" {
+    source                 = "./api-gw-resource"
+    apigw_id               = aws_api_gateway_rest_api.langevals.id
+    apigw_root_resource_id = aws_api_gateway_resource.product_sentiment_polarity.id
+    path                   = "evaluate"
+    method                 = "POST"
+
+    lambda_invoke_arn = module.langevals-evaluator.lambda_invoke_arn
+}
+
 resource "aws_api_gateway_resource" "similarity" {
     rest_api_id = aws_api_gateway_rest_api.langevals.id
     parent_id   = aws_api_gateway_resource.langevals.id
@@ -427,11 +443,11 @@ resource "aws_api_gateway_deployment" "this" {
     count = module.variables.profile == "lw-prod" ? 1 : 0
 
     triggers = {
-        redeployment = sha1(jsonencode([module.aws-comprehend_pii_detection-api-gw, module.lingua-language_detection-api-gw, module.azure-content_safety-api-gw, module.azure-jailbreak-api-gw, module.ragas-answer_relevancy-api-gw, module.ragas-context_precision-api-gw, module.ragas-context_recall-api-gw, module.ragas-context_relevancy-api-gw, module.ragas-context_utilization-api-gw, module.ragas-faithfulness-api-gw, module.langevals-basic-api-gw, module.langevals-competitor_blocklist-api-gw, module.langevals-competitor_llm-api-gw, module.langevals-llm_boolean-api-gw, module.langevals-llm_score-api-gw, module.langevals-off_topic-api-gw, module.langevals-similarity-api-gw, module.openai-moderation-api-gw, module.huggingface-llama_guard-api-gw, module.google_cloud-dlp_pii_detection-api-gw]))
+        redeployment = sha1(jsonencode([module.aws-comprehend_pii_detection-api-gw, module.lingua-language_detection-api-gw, module.azure-content_safety-api-gw, module.azure-jailbreak-api-gw, module.ragas-answer_relevancy-api-gw, module.ragas-context_precision-api-gw, module.ragas-context_recall-api-gw, module.ragas-context_relevancy-api-gw, module.ragas-context_utilization-api-gw, module.ragas-faithfulness-api-gw, module.langevals-basic-api-gw, module.langevals-competitor_blocklist-api-gw, module.langevals-competitor_llm-api-gw, module.langevals-llm_boolean-api-gw, module.langevals-llm_score-api-gw, module.langevals-off_topic-api-gw, module.langevals-product_sentiment_polarity-api-gw, module.langevals-similarity-api-gw, module.openai-moderation-api-gw, module.huggingface-llama_guard-api-gw, module.google_cloud-dlp_pii_detection-api-gw]))
     }
 
     depends_on = [
-        module.aws-comprehend_pii_detection-api-gw, module.lingua-language_detection-api-gw, module.azure-content_safety-api-gw, module.azure-jailbreak-api-gw, module.ragas-answer_relevancy-api-gw, module.ragas-context_precision-api-gw, module.ragas-context_recall-api-gw, module.ragas-context_relevancy-api-gw, module.ragas-context_utilization-api-gw, module.ragas-faithfulness-api-gw, module.langevals-basic-api-gw, module.langevals-competitor_blocklist-api-gw, module.langevals-competitor_llm-api-gw, module.langevals-llm_boolean-api-gw, module.langevals-llm_score-api-gw, module.langevals-off_topic-api-gw, module.langevals-similarity-api-gw, module.openai-moderation-api-gw, module.huggingface-llama_guard-api-gw, module.google_cloud-dlp_pii_detection-api-gw
+        module.aws-comprehend_pii_detection-api-gw, module.lingua-language_detection-api-gw, module.azure-content_safety-api-gw, module.azure-jailbreak-api-gw, module.ragas-answer_relevancy-api-gw, module.ragas-context_precision-api-gw, module.ragas-context_recall-api-gw, module.ragas-context_relevancy-api-gw, module.ragas-context_utilization-api-gw, module.ragas-faithfulness-api-gw, module.langevals-basic-api-gw, module.langevals-competitor_blocklist-api-gw, module.langevals-competitor_llm-api-gw, module.langevals-llm_boolean-api-gw, module.langevals-llm_score-api-gw, module.langevals-off_topic-api-gw, module.langevals-product_sentiment_polarity-api-gw, module.langevals-similarity-api-gw, module.openai-moderation-api-gw, module.huggingface-llama_guard-api-gw, module.google_cloud-dlp_pii_detection-api-gw
     ]
 
     rest_api_id = aws_api_gateway_rest_api.langevals.id
