7d760de23e5f5c32d91b5a49c0585391d707d911
Richard Huth <richhuth@gmail.com>
2024-06-13 15:29:14 +0200

    Google fix

--- src/components/ExtraFooterComponents.tsx
+++ src/components/ExtraFooterComponents.tsx
@@ -1,4 +1,3 @@
-import Script from "next/script";
 import { useEffect } from "react";
 import { useOrganizationTeamProject } from "../../langwatch/langwatch/src/hooks/useOrganizationTeamProject";
 import { useRequiredSession } from "../../langwatch/langwatch/src/hooks/useRequiredSession";
@@ -24,23 +23,5 @@ export function ExtraFooterComponents() {
     });
   }, [organization?.id, project?.id]);
 
-  return (
-    <>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-0VEKZY9DMY"
-      ></Script>
-      <Script>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-0VEKZY9DMY');
-        `}
-      </Script>
-      <Script id="crisp">
-        {`window.$crisp=[];window.CRISP_WEBSITE_ID="cca9eacd-c4d6-4258-a7fc-9606be6fd012";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`}
-      </Script>
-    </>
-  );
+  return <></>;
 }
