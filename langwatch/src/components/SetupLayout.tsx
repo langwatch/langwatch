1a7e5b67eaa111c638d0d274e3cd0cddeeca7c72
Richard Huth <richhuth@gmail.com>
2024-06-13 14:13:25 +0200

    Add logout to registration

--- langwatch/src/components/SetupLayout.tsx
+++ langwatch/src/components/SetupLayout.tsx
@@ -1,6 +1,16 @@
-import { Box, Card, CardBody, Container } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  Container,
+  HStack,
+  Spacer,
+  Button,
+} from "@chakra-ui/react";
 import Head from "next/head";
 import { type PropsWithChildren } from "react";
+import { signOut } from "next-auth/react";
+import { LogOut } from "react-feather";
 
 export const SetupLayout = ({ children }: PropsWithChildren) => {
   return (
@@ -14,6 +24,12 @@ export const SetupLayout = ({ children }: PropsWithChildren) => {
       <Head>
         <title>LangWatch - Setup</title>
       </Head>
+      <HStack position="fixed" top={2} right={2} zIndex={99}>
+        <Spacer />
+        <Button variant="ghost" onClick={() => void signOut()}>
+          <LogOut />
+        </Button>
+      </HStack>
       <Container>
         <Card>
           <CardBody>{children}</CardBody>
