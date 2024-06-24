025f16fd931799496d5a00421564ba1e03e359fe
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-06-24 17:11:14 +0200

    Fix cpauting open_dashboard events

--- src/components/ExtraFooterComponents.tsx
+++ src/components/ExtraFooterComponents.tsx
@@ -1,8 +1,36 @@
 import { useEffect } from "react";
 import { useOrganizationTeamProject } from "../../langwatch/langwatch/src/hooks/useOrganizationTeamProject";
 import { useRequiredSession } from "../../langwatch/langwatch/src/hooks/useRequiredSession";
+import Script from "next/script";
 
 export function ExtraFooterComponents() {
+  const session = useRequiredSession({
+    required: false,
+  });
+
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-0VEKZY9DMY"
+      ></Script>
+      <Script id="google-analytics">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-0VEKZY9DMY');
+    `}
+      </Script>
+      <Script id="crisp">
+        {`window.$crisp=[];window.CRISP_WEBSITE_ID="cca9eacd-c4d6-4258-a7fc-9606be6fd012";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`}
+      </Script>
+      {session.data?.user ? <SignedInExtraFooterComponents /> : null}
+    </>
+  );
+}
+
+export function SignedInExtraFooterComponents() {
   const session = useRequiredSession();
   const { organization, project } = useOrganizationTeamProject({
     redirectToOnboarding: false,
@@ -23,5 +51,5 @@ export function ExtraFooterComponents() {
     });
   }, [organization?.id, project?.id]);
 
-  return <></>;
+  return null;
 }
