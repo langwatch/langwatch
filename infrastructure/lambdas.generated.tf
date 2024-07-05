aee93fb5910e390d798ebb178fe68aa7311d56e2
Eugenumber1 <y.budnyk789@gmail.com>
2024-07-05 15:35:24 +0200

    Deploy the new evaluator Prompt Injection, adjusted infra file-lambda generated file

--- infrastructure/lambdas.generated.tf
+++ infrastructure/lambdas.generated.tf
@@ -65,59 +65,6 @@ resource "aws_api_gateway_resource" "lingua" {
     path_part   = "lingua"
 }
 
-resource "aws_api_gateway_resource" "azure-content_safety" {
-    rest_api_id = aws_api_gateway_rest_api.langevals.id
-    parent_id   = aws_api_gateway_resource.azure.id
-    path_part   = "content_safety"
-}
-
-module "azure-content_safety-api-gw" {
-    source                 = "./api-gw-resource"
-    apigw_id               = aws_api_gateway_rest_api.langevals.id
-    apigw_root_resource_id = aws_api_gateway_resource.azure-content_safety.id
-    path                   = "evaluate"
-    method                 = "POST"
-
-    lambda_invoke_arn = module.azure-evaluator.lambda_invoke_arn
-
-    depends_on = [
-        aws_api_gateway_resource.azure-content_safety
-    ]
-}
-
-resource "aws_api_gateway_resource" "azure-jailbreak" {
-    rest_api_id = aws_api_gateway_rest_api.langevals.id
-    parent_id   = aws_api_gateway_resource.azure.id
-    path_part   = "jailbreak"
-}
-
-module "azure-jailbreak-api-gw" {
-    source                 = "./api-gw-resource"
-    apigw_id               = aws_api_gateway_rest_api.langevals.id
-    apigw_root_resource_id = aws_api_gateway_resource.azure-jailbreak.id
-    path                   = "evaluate"
-    method                 = "POST"
-
-    lambda_invoke_arn = module.azure-evaluator.lambda_invoke_arn
-
-    depends_on = [
-        aws_api_gateway_resource.azure-jailbreak
-    ]
-}
-
-module "azure-evaluator" {
-    source              = "./lambda"
-    evaluator_package   = "azure"
-    sns_alarms_topic_arn = aws_sns_topic.alarms.arn
-    apigw_execution_arn = aws_api_gateway_rest_api.langevals.execution_arn
-}
-
-resource "aws_api_gateway_resource" "azure" {
-    rest_api_id = aws_api_gateway_rest_api.langevals.id
-    parent_id   = aws_api_gateway_rest_api.langevals.root_resource_id
-    path_part   = "azure"
-}
-
 resource "aws_api_gateway_resource" "ragas-answer_relevancy" {
     rest_api_id = aws_api_gateway_rest_api.langevals.id
     parent_id   = aws_api_gateway_resource.ragas.id
@@ -249,6 +196,79 @@ resource "aws_api_gateway_resource" "ragas" {
     rest_api_id = aws_api_gateway_rest_api.langevals.id
     parent_id   = aws_api_gateway_rest_api.langevals.root_resource_id
     path_part   = "ragas"
+}
+
+resource "aws_api_gateway_resource" "azure-content_safety" {
+    rest_api_id = aws_api_gateway_rest_api.langevals.id
+    parent_id   = aws_api_gateway_resource.azure.id
+    path_part   = "content_safety"
+}
+
+module "azure-content_safety-api-gw" {
+    source                 = "./api-gw-resource"
+    apigw_id               = aws_api_gateway_rest_api.langevals.id
+    apigw_root_resource_id = aws_api_gateway_resource.azure-content_safety.id
+    path                   = "evaluate"
+    method                 = "POST"
+
+    lambda_invoke_arn = module.azure-evaluator.lambda_invoke_arn
+
+    depends_on = [
+        aws_api_gateway_resource.azure-content_safety
+    ]
+}
+
+resource "aws_api_gateway_resource" "azure-jailbreak" {
+    rest_api_id = aws_api_gateway_rest_api.langevals.id
+    parent_id   = aws_api_gateway_resource.azure.id
+    path_part   = "jailbreak"
+}
+
+module "azure-jailbreak-api-gw" {
+    source                 = "./api-gw-resource"
+    apigw_id               = aws_api_gateway_rest_api.langevals.id
+    apigw_root_resource_id = aws_api_gateway_resource.azure-jailbreak.id
+    path                   = "evaluate"
+    method                 = "POST"
+
+    lambda_invoke_arn = module.azure-evaluator.lambda_invoke_arn
+
+    depends_on = [
+        aws_api_gateway_resource.azure-jailbreak
+    ]
+}
+
+resource "aws_api_gateway_resource" "azure-prompt_injection" {
+    rest_api_id = aws_api_gateway_rest_api.langevals.id
+    parent_id   = aws_api_gateway_resource.azure.id
+    path_part   = "prompt_injection"
+}
+
+module "azure-prompt_injection-api-gw" {
+    source                 = "./api-gw-resource"
+    apigw_id               = aws_api_gateway_rest_api.langevals.id
+    apigw_root_resource_id = aws_api_gateway_resource.azure-prompt_injection.id
+    path                   = "evaluate"
+    method                 = "POST"
+
+    lambda_invoke_arn = module.azure-evaluator.lambda_invoke_arn
+
+    depends_on = [
+        aws_api_gateway_resource.azure-prompt_injection
+    ]
+}
+
+module "azure-evaluator" {
+    source              = "./lambda"
+    evaluator_package   = "azure"
+    sns_alarms_topic_arn = aws_sns_topic.alarms.arn
+    apigw_execution_arn = aws_api_gateway_rest_api.langevals.execution_arn
+}
+
+resource "aws_api_gateway_resource" "azure" {
+    rest_api_id = aws_api_gateway_rest_api.langevals.id
+    parent_id   = aws_api_gateway_rest_api.langevals.root_resource_id
+    path_part   = "azure"
 }
 
 resource "aws_api_gateway_resource" "langevals-basic" {
@@ -580,11 +600,11 @@ resource "aws_api_gateway_deployment" "this" {
     count = module.variables.profile == "lw-prod" ? 1 : 0
 
     triggers = {
-        redeployment = sha1(jsonencode([module.aws-comprehend_pii_detection-api-gw, module.lingua-language_detection-api-gw, module.azure-content_safety-api-gw, module.azure-jailbreak-api-gw, module.ragas-answer_relevancy-api-gw, module.ragas-context_precision-api-gw, module.ragas-context_recall-api-gw, module.ragas-context_relevancy-api-gw, module.ragas-context_utilization-api-gw, module.ragas-faithfulness-api-gw, module.langevals-basic-api-gw, module.langevals-competitor_blocklist-api-gw, module.langevals-competitor_llm-api-gw, module.langevals-competitor_llm_function_call-api-gw, module.langevals-llm_boolean-api-gw, module.langevals-llm_score-api-gw, module.langevals-off_topic-api-gw, module.langevals-product_sentiment_polarity-api-gw, module.langevals-similarity-api-gw, module.openai-moderation-api-gw, module.huggingface-llama_guard-api-gw, module.haystack-faithfulness-api-gw, module.google_cloud-dlp_pii_detection-api-gw]))
+        redeployment = sha1(jsonencode([module.aws-comprehend_pii_detection-api-gw, module.lingua-language_detection-api-gw, module.ragas-answer_relevancy-api-gw, module.ragas-context_precision-api-gw, module.ragas-context_recall-api-gw, module.ragas-context_relevancy-api-gw, module.ragas-context_utilization-api-gw, module.ragas-faithfulness-api-gw, module.azure-content_safety-api-gw, module.azure-jailbreak-api-gw, module.azure-prompt_injection-api-gw, module.langevals-basic-api-gw, module.langevals-competitor_blocklist-api-gw, module.langevals-competitor_llm-api-gw, module.langevals-competitor_llm_function_call-api-gw, module.langevals-llm_boolean-api-gw, module.langevals-llm_score-api-gw, module.langevals-off_topic-api-gw, module.langevals-product_sentiment_polarity-api-gw, module.langevals-similarity-api-gw, module.openai-moderation-api-gw, module.huggingface-llama_guard-api-gw, module.haystack-faithfulness-api-gw, module.google_cloud-dlp_pii_detection-api-gw]))
     }
 
     depends_on = [
-        module.aws-comprehend_pii_detection-api-gw, module.lingua-language_detection-api-gw, module.azure-content_safety-api-gw, module.azure-jailbreak-api-gw, module.ragas-answer_relevancy-api-gw, module.ragas-context_precision-api-gw, module.ragas-context_recall-api-gw, module.ragas-context_relevancy-api-gw, module.ragas-context_utilization-api-gw, module.ragas-faithfulness-api-gw, module.langevals-basic-api-gw, module.langevals-competitor_blocklist-api-gw, module.langevals-competitor_llm-api-gw, module.langevals-competitor_llm_function_call-api-gw, module.langevals-llm_boolean-api-gw, module.langevals-llm_score-api-gw, module.langevals-off_topic-api-gw, module.langevals-product_sentiment_polarity-api-gw, module.langevals-similarity-api-gw, module.openai-moderation-api-gw, module.huggingface-llama_guard-api-gw, module.haystack-faithfulness-api-gw, module.google_cloud-dlp_pii_detection-api-gw
+        module.aws-comprehend_pii_detection-api-gw, module.lingua-language_detection-api-gw, module.ragas-answer_relevancy-api-gw, module.ragas-context_precision-api-gw, module.ragas-context_recall-api-gw, module.ragas-context_relevancy-api-gw, module.ragas-context_utilization-api-gw, module.ragas-faithfulness-api-gw, module.azure-content_safety-api-gw, module.azure-jailbreak-api-gw, module.azure-prompt_injection-api-gw, module.langevals-basic-api-gw, module.langevals-competitor_blocklist-api-gw, module.langevals-competitor_llm-api-gw, module.langevals-competitor_llm_function_call-api-gw, module.langevals-llm_boolean-api-gw, module.langevals-llm_score-api-gw, module.langevals-off_topic-api-gw, module.langevals-product_sentiment_polarity-api-gw, module.langevals-similarity-api-gw, module.openai-moderation-api-gw, module.huggingface-llama_guard-api-gw, module.haystack-faithfulness-api-gw, module.google_cloud-dlp_pii_detection-api-gw
     ]
 
     rest_api_id = aws_api_gateway_rest_api.langevals.id
