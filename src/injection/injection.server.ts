47be07199164208f109a2a0807e061f57e4cf651
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-07-06 10:55:29 +0200

    Disable also stripe mandatory env protection

--- src/injection/injection.server.ts
+++ src/injection/injection.server.ts
@@ -10,12 +10,6 @@ import { subscriptionRouter } from "../pages/extra_api/api/subscription";
 import { PostRegistrationCallback } from "../postRegistrationCallback";
 import demoBot from "../pages/extra_api/demo/hotel_bot";
 
-if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_WEBHOOK_SECRET) {
-  throw new Error(
-    "STRIPE_SECRET_KEY and STRIPE_WEBHOOK_SECRET env vars must be set"
-  );
-}
-
 const dependencies: Dependencies = {
   subscriptionHandler: SubscriptionHandlerSaas,
   sessionHandler: async ({ req, session, user }) => {
