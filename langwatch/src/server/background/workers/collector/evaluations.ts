e64beae544b7b6e4f21f0b403f7358f46169c989
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-08-12 00:58:39 +0200

    Use md5 as the autogenerated eval id to avoid duplication in case of resending the same trace or duplicated evaluations

--- langwatch/src/server/background/workers/collector/evaluations.ts
+++ langwatch/src/server/background/workers/collector/evaluations.ts
@@ -1,17 +1,22 @@
-import { nanoid } from "nanoid";
 import type { ElasticSearchEvaluation } from "../../../tracer/types";
 import type { CollectorJob } from "../../types";
 import { elasticSearchEvaluationSchema } from "../../../tracer/types.generated";
+import crypto from "crypto";
 
 export const mapEvaluations = (
   data: CollectorJob
 ): ElasticSearchEvaluation[] | undefined => {
-  return data.evaluations?.map((evaluation) => {
+  const evaluations = data.evaluations?.map((evaluation) => {
+    const evaluationMD5 = crypto
+      .createHash("md5")
+      .update(JSON.stringify(evaluation))
+      .digest("hex");
+
     const evaluation_: ElasticSearchEvaluation = {
       ...evaluation,
       trace_id: data.traceId,
       project_id: data.projectId,
-      check_id: evaluation.evaluation_id ?? `eval_${nanoid()}`,
+      check_id: evaluation.evaluation_id ?? `eval_md5_${evaluationMD5}`,
       check_type: evaluation.type,
       check_name: evaluation.name,
       status: evaluation.status ?? (evaluation.error ? "error" : "processed"),
@@ -25,4 +30,14 @@ export const mapEvaluations = (
     // reparse to remove unwanted extraneous fields
     return elasticSearchEvaluationSchema.parse(evaluation_);
   });
+
+  const uniqueByCheckIdKeepingLast = evaluations
+    ?.reverse()
+    .filter(
+      (evaluation, index, self) =>
+        index === self.findIndex((t) => t.check_id === evaluation.check_id)
+    )
+    .reverse();
+
+  return uniqueByCheckIdKeepingLast;
 };
