c5e379079980f05978cabff5c37c8d735a04bdd9
Richard Huth <richhuth@gmail.com>
2024-08-13 13:39:05 +0200

    No impersonator

--- src/components/ExtraFooterComponents.tsx
+++ src/components/ExtraFooterComponents.tsx
@@ -22,23 +22,23 @@ export function ExtraFooterComponents() {
 
   return (
     <>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-0VEKZY9DMY"
-      ></Script>
-      <Script id="google-analytics">
-        {`
+      {(session.data.user as any).impersonator ? null : (
+        <>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-0VEKZY9DMY"
+          ></Script>
+          <Script id="google-analytics">
+            {`
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'G-0VEKZY9DMY');
     `}
-      </Script>
-      <Script id="crisp">
-        {`window.$crisp=[];window.CRISP_WEBSITE_ID="cca9eacd-c4d6-4258-a7fc-9606be6fd012";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`}
-      </Script>
-      <Script id="pendo">
-        {`(function(apiKey){
+          </Script>
+
+          <Script id="pendo">
+            {`(function(apiKey){
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
     v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
         o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
@@ -52,13 +52,18 @@ export function ExtraFooterComponents() {
             name: "${session.data.user?.name || ""}"
         },
         account: {
-            id: "${organization?.id || ""}",
-            projectName: "${project?.name || ""}",
-            organizationName: "${organization?.name || ""}"
+            id: "${organization.id || ""}",
+            projectName: "${project.name || ""}",
+            organizationName: "${organization.name || ""}"
         }
     });
 })('18f008fe-1a55-4b22-70d9-964d6e98b130');`}
-      </Script>
+          </Script>
+          <Script id="crisp">
+            {`window.$crisp=[];window.CRISP_WEBSITE_ID="cca9eacd-c4d6-4258-a7fc-9606be6fd012";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`}
+          </Script>
+        </>
+      )}
       {session.data?.user ? <SignedInExtraFooterComponents /> : null}
     </>
   );
