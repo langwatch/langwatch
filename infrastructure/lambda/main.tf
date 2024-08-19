8e920c88c03001bec7522588cca02812eea97ac3
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-08-19 14:42:21 +0200

    Send to slack also when lambda alarm is back to normal

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -43,18 +43,20 @@ resource "aws_lambda_function" "this" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "lambda_function_errors" {
-  count               = module.variables.profile == "lw-prod" ? 1 : 0
-  alarm_name          = "${local.evaluator_package}-lambda-errors"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "Errors"
-  namespace           = "AWS/Lambda"
-  period              = "60"
-  statistic           = "Sum"
-  threshold           = "1"
-  alarm_description   = "Alarm when ${local.evaluator_package} lambda has errors"
-  actions_enabled     = true
-  alarm_actions       = [var.sns_alarms_topic_arn]
+  count                     = module.variables.profile == "lw-prod" ? 1 : 0
+  alarm_name                = "${local.evaluator_package}-lambda-errors"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "Errors"
+  namespace                 = "AWS/Lambda"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "Alarm when ${local.evaluator_package} lambda has errors"
+  actions_enabled           = true
+  alarm_actions             = [var.sns_alarms_topic_arn]
+  ok_actions                = [var.sns_alarms_topic_arn]
+  insufficient_data_actions = [var.sns_alarms_topic_arn]
 
   dimensions = {
     FunctionName = aws_lambda_function.this[0].function_name
