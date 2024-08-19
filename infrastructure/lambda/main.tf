11cd7a26ae933bebfedbe1c983f5edbbf6c15b4c
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-08-19 14:40:12 +0200

    Fix lambda invocation body, apparently it expects a sourceIp and stuff

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -231,10 +231,22 @@ resource "aws_cloudwatch_event_target" "keep_warm" {
       "content-type" : "application/json"
     },
     "requestContext" : {
+      "accountId" : "123456789012",
+      "apiId" : "api-id",
+      "domainName" : "id.execute-api.us-east-1.amazonaws.com",
+      "domainPrefix" : "id",
       "http" : {
         "method" : "GET",
-        "path" : "/healthcheck"
-      }
+        "path" : "/healthcheck",
+        "protocol" : "HTTP/1.1",
+        "sourceIp" : "192.168.0.1",
+        "userAgent" : "agent"
+      },
+      "requestId" : "id",
+      "routeKey" : "GET /healthcheck",
+      "stage" : "$default",
+      "time" : "12/Mar/2020:19:03:58 +0000",
+      "timeEpoch" : 1583348638390
     },
     "isBase64Encoded" : false
   })
