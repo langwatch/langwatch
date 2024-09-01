4eda4f7d04562110d0fad993e8f73e5531baf9cd
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-09-01 20:01:18 +0200

    Fix double calling upgrade warnings

--- langwatch/src/start.js
+++ langwatch/src/start.js
@@ -43,26 +43,31 @@ module.exports.startApp = async (dir = path.dirname(__dirname)) => {
     }
   });
 
-  server.on("upgrade", (req, socket, head) => {
+  const upgradeListener = (defaultHandler) => (req, socket, head) => {
     const parsedUrl = parse(req.url ?? "", true);
 
     // Pass hot module reloading requests to Next.js
     if (parsedUrl.pathname === "/_next/webpack-hmr") {
-      void upgradeHandler(req, socket, head);
+      void defaultHandler(req, socket, head);
     } else if (parsedUrl.pathname?.startsWith("/api/studio/ws")) {
-      // TODO: handle studio websocket
       void studioSocket.handleUpgrade(req, socket, head, parsedUrl);
     } else {
       socket.destroy();
     }
-  });
+  };
 
+  const initialHandler = upgradeListener(upgradeHandler);
+  server.on("upgrade", initialHandler);
+
+  // Workaround because apparently next.js calls .on("upgrade", ...) internally,
+  // overwriting the initialHandler, we need to re - attach it while keeping hmr working
   const originalOn = server.on.bind(server);
-  server.on = (event, listener) => {
+  server.on = (event, handler) => {
     if (event === "upgrade") {
-      return;
+      server.off("upgrade", initialHandler);
+      return originalOn(event, upgradeListener(handler));
     }
-    return originalOn(event, listener);
+    return originalOn(event, handler);
   };
 
   server.once("error", (err) => {
