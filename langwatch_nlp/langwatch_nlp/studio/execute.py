1b812d83a05250f970e31429f42f7ca595a29b46
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-09-03 15:16:23 +0200

    Run each event execution on a separate multiprocess, and switch to uvicorn instead of hypercorn to be able to do that, since hypercorn uses multiprocessing for the workers already, and workers cannot start subprocess within, believe me, I tried. Uvicorn uses multithreading, so we can start multiprocess within, and we really need this isolation, so this works all fine!

--- langwatch_nlp/langwatch_nlp/studio/execute.py
+++ langwatch_nlp/langwatch_nlp/studio/execute.py
@@ -1,6 +1,8 @@
 import asyncio
+from multiprocessing import Process, Queue
+from queue import Empty
 import time
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Dict
 from fastapi import FastAPI, Response, BackgroundTasks
 from fastapi.responses import StreamingResponse
 import json
@@ -91,6 +93,81 @@ async def execute_event(
     yield Done()
 
 
+def execute_event_to_queue(event: StudioClientEvent, queue: "Queue[StudioServerEvent]"):
+    async def async_execute_event():
+        async for event_ in execute_event(event):
+            queue.put(event_)
+
+    asyncio.run(async_execute_event())
+
+
+running_processes: Dict[str, Process] = {}
+
+
+# We execute events on a subprocess because each user might execute completely different code,
+# which can alter the global Python interpreter state in unpredictable ways. DSPy itself does
+# a lot of this. At same time, we want to fork the main process to avoid double RAM spending and
+# startup times.
+async def execute_event_on_a_subprocess(event: StudioClientEvent):
+    queue: "Queue[StudioServerEvent]" = Queue()
+
+    # ctx = multiprocessing.get_context("spawn")
+    process = Process(target=execute_event_to_queue, args=(event, queue))
+    # process = spawn_process.
+    process.start()
+    if (
+        hasattr(event.payload, "trace_id")
+        and event.payload.trace_id not in running_processes
+    ):
+        running_processes[event.payload.trace_id] = process
+
+    timeout_without_messages = 120  # seconds
+
+    try:
+        done = False
+        last_message_time = time.time()
+        time_since_last_message = 0
+        while time_since_last_message < timeout_without_messages:
+            time_since_last_message = time.time() - last_message_time
+            try:
+                result = queue.get(timeout=0.1)
+                yield result
+                last_message_time = time.time()
+
+                if isinstance(result, Done):
+                    done = True
+                    break
+            except Empty:
+                if timeout_without_messages > 10 and not process.is_alive():
+                    raise Exception("Runtime crashed")
+
+                await asyncio.sleep(0.1)
+
+        if not done:
+            # Timeout occurred
+            yield Error(payload=ErrorPayload(message="Execution timed out"))
+            process.terminate()
+            process.join(timeout=5)  # Give it 5 seconds to terminate gracefully
+            if process.is_alive():
+                # Force kill if it doesn't terminate
+                process.kill()
+                process.join()
+
+    except Exception as e:
+        yield Error(payload=ErrorPayload(message=f"Unexpected error: {repr(e)}"))
+    finally:
+        # Ensure the process is terminated and resources are cleaned up
+        if process.is_alive():
+            process.terminate()
+            process.join()
+
+        if (
+            hasattr(event.payload, "trace_id")
+            and event.payload.trace_id in running_processes
+        ):
+            del running_processes[event.payload.trace_id]
+
+
 async def event_encoder(event_generator: AsyncGenerator[StudioServerEvent, None]):
     async for event in event_generator:
         yield f"data: {json.dumps(event.model_dump(exclude_none=True))}\n\n"
@@ -102,5 +179,6 @@ async def execute(
 ):
     response.headers["Cache-Control"] = "no-cache"
     return StreamingResponse(
-        event_encoder(execute_event(event)), media_type="text/event-stream"
+        event_encoder(execute_event_on_a_subprocess(event)),
+        media_type="text/event-stream",
     )
