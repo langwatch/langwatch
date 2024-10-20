886c79b35a2ca70071c9a9d9f421b22583f6c986
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-10-20 23:55:28 +0200

    Remove langwatch_nlp task healthcheck, seems to be causing more harm than good

--- infrastructure/langwatch_nlp.tf
+++ infrastructure/langwatch_nlp.tf
@@ -56,13 +56,6 @@ resource "aws_ecs_task_definition" "langwatch_nlp" {
           awslogs-stream-prefix = "langwatch_nlp"
         }
       }
-      healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:8080/docs || exit 1"]
-        interval    = 30
-        timeout     = 5
-        retries     = 3
-        startPeriod = 15
-      }
     }
   ])
 
