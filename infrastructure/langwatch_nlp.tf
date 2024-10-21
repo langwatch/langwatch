4b591e6786f6480495cf393e30e4dae63f888dd1
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-10-21 14:48:56 +0200

    Bump langwatch, increase cpu back again, let's see...

--- infrastructure/langwatch_nlp.tf
+++ infrastructure/langwatch_nlp.tf
@@ -27,7 +27,7 @@ resource "aws_ecs_task_definition" "langwatch_nlp" {
   family                   = "langwatch-nlp-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 1024
+  cpu                      = 2048
   memory                   = 2048
   execution_role_arn       = aws_iam_role.ecs_task_execution_role_langwatch_nlp.arn
   task_role_arn            = aws_iam_role.ecs_task_role_langwatch_nlp.arn
@@ -36,7 +36,7 @@ resource "aws_ecs_task_definition" "langwatch_nlp" {
     {
       name      = "langwatch_nlp"
       image     = "${aws_ecr_repository.langwatch_nlp.repository_url}:${local.langwatch_nlp_tag}"
-      cpu       = 1024
+      cpu       = 2048
       memory    = 2048
       essential = true
       portMappings = [
