e3fffa578d8f5303ec8662a581ca27a224d6bb42
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-10-21 13:24:25 +0200

    Oops, need to increase the task as well

--- infrastructure/langwatch_nlp.tf
+++ infrastructure/langwatch_nlp.tf
@@ -27,7 +27,7 @@ resource "aws_ecs_task_definition" "langwatch_nlp" {
   family                   = "langwatch-nlp-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 1024
+  cpu                      = 2048
   memory                   = 2048
   execution_role_arn       = aws_iam_role.ecs_task_execution_role_langwatch_nlp.arn
   task_role_arn            = aws_iam_role.ecs_task_role_langwatch_nlp.arn
