6dcfa4299bdca0cdb01912190542aaf7d8bf7fff
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-10-21 12:25:06 +0200

    Conflicting global namespace

--- infrastructure/langwatch_nlp.tf
+++ infrastructure/langwatch_nlp.tf
@@ -83,7 +83,7 @@ resource "aws_iam_role" "ecs_task_role_langwatch_nlp" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_role_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "ecs_langwatch_nlp_task_role_policy_attachment" {
   role       = aws_iam_role.ecs_task_role_langwatch_nlp.name
   policy_arn = aws_iam_policy.ecs_exec_policy_langwatch_nlp.arn
 }
