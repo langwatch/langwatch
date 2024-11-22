5c289687a35cf2f02cfe62a157e701febfe84702
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-11-22 16:25:05 +0100

    Add langevals

---
+++ onprem/gcp/langevals.tf
@@ -0,0 +1,90 @@
+# Read version from package.json
+data "local_file" "package_json" {
+  filename = "${path.module}/../../package.json"
+}
+
+locals {
+  langwatch_version = jsondecode(data.local_file.package_json.content).version
+}
+
+# LangEvals Deployment
+resource "kubernetes_deployment" "langevals" {
+  metadata {
+    name = "langevals"
+    annotations = {
+      "deployment-timestamp" = timestamp()
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "langevals"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "langevals"
+        }
+      }
+
+      spec {
+        container {
+          name  = "langevals"
+          image = "339712859611.dkr.ecr.eu-central-1.amazonaws.com/onprem_langevals:${local.langwatch_version}"
+
+          port {
+            container_port = 8000
+          }
+
+          resources {
+            requests = {
+              cpu    = "2"
+              memory = "4Gi"
+            }
+            limits = {
+              cpu    = "2"
+              memory = "4Gi"
+            }
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    google_container_cluster.primary,
+    google_container_node_pool.primary_nodes
+  ]
+}
+
+# LangEvals Service
+resource "kubernetes_service" "langevals" {
+  metadata {
+    name = "langevals-service"
+    annotations = {
+      "deployment-timestamp" = timestamp()
+    }
+  }
+
+  spec {
+    selector = {
+      app = "langevals"
+    }
+
+    port {
+      port        = 80
+      target_port = 8000
+    }
+
+    type = "ClusterIP"
+  }
+
+  depends_on = [
+    kubernetes_deployment.langevals
+  ]
+}
