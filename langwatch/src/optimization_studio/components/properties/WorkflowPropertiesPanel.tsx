e2c4b78e7973143e31ae86d6a3b8df3ce8d88150
Richard Huth <richhuth@gmail.com>
2024-11-22 13:27:17 +0100

    Workflow Name and Description update

--- langwatch/src/optimization_studio/components/properties/WorkflowPropertiesPanel.tsx
+++ langwatch/src/optimization_studio/components/properties/WorkflowPropertiesPanel.tsx
@@ -1,9 +1,18 @@
-import { Box, Text, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Input,
+  Text,
+  Textarea,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
 import { useWorkflowStore } from "../../hooks/useWorkflowStore";
 import { BasePropertiesPanel, PropertyField } from "./BasePropertiesPanel";
 import { LLMConfigField } from "./modals/LLMConfigModal";
 import { WorkflowIcon } from "../ColorfulBlockIcons";
 import { EmojiPickerModal } from "./modals/EmojiPickerModal";
+import { useState } from "react";
 
 export function WorkflowPropertiesPanel() {
   const { getWorkflow, setWorkflow } = useWorkflowStore(
@@ -14,6 +23,10 @@ export function WorkflowPropertiesPanel() {
   );
 
   const workflow = getWorkflow();
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [name, setName] = useState<string | undefined>(undefined);
+  const [isEditingDescription, setIsEditingDescription] = useState(false);
+  const [description, setDescription] = useState<string | undefined>(undefined);
 
   const { isOpen, onClose, onToggle } = useDisclosure();
 
@@ -22,19 +35,114 @@ export function WorkflowPropertiesPanel() {
       node={workflow}
       header={
         <>
-          <EmojiPickerModal
-            isOpen={isOpen}
-            onClose={onClose}
-            onChange={(emoji) => {
-              setWorkflow({ icon: emoji });
-            }}
-          />
-          <Box role="button" cursor="pointer" onClick={onToggle}>
-            <WorkflowIcon icon={workflow.icon} size="lg" />
-          </Box>
-          <Text fontSize={16} fontWeight={500}>
-            {workflow.name}
-          </Text>
+          <HStack align="center" spacing={2}>
+            <VStack align="flex-start">
+              <HStack align="center">
+                <EmojiPickerModal
+                  isOpen={isOpen}
+                  onClose={onClose}
+                  onChange={(emoji) => {
+                    setWorkflow({ icon: emoji });
+                  }}
+                />
+                <Box
+                  role="button"
+                  cursor="pointer"
+                  onClick={onToggle}
+                  paddingTop={1}
+                >
+                  <WorkflowIcon icon={workflow.icon} size="lg" />
+                </Box>
+
+                {isEditingName ? (
+                  <Input
+                    fontSize={15}
+                    marginLeft={1}
+                    fontWeight={500}
+                    variant="outline"
+                    background="transparent"
+                    value={name ?? workflow.name}
+                    borderRadius={5}
+                    paddingLeft={1}
+                    margin={0}
+                    size="sm"
+                    onBlur={() => {
+                      setIsEditingName(false);
+                      if (name) {
+                        setWorkflow({ name });
+                      }
+                    }}
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        setIsEditingName(false);
+                        if (name) {
+                          setWorkflow({ name });
+                        }
+                      }
+                    }}
+                  />
+                ) : (
+                  <Text
+                    fontSize={16}
+                    fontWeight={500}
+                    onClick={() => {
+                      setIsEditingName(true);
+                      setIsEditingDescription(false);
+                    }}
+                  >
+                    {workflow.name}
+                  </Text>
+                )}
+              </HStack>
+
+              {isEditingDescription ? (
+                <Textarea
+                  fontSize={12}
+                  marginLeft={1}
+                  fontWeight={300}
+                  variant="outline"
+                  background="transparent"
+                  value={description ?? workflow.description}
+                  borderRadius={5}
+                  paddingLeft={1}
+                  margin={0}
+                  width="300px"
+                  size="sm"
+                  onBlur={() => {
+                    setIsEditingDescription(false);
+                    if (description) {
+                      setWorkflow({ description });
+                    }
+                  }}
+                  onChange={(e) => {
+                    setDescription(e.target.value);
+                  }}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      setIsEditingDescription(false);
+                      if (description) {
+                        setWorkflow({ description });
+                      }
+                    }
+                  }}
+                />
+              ) : (
+                <Text
+                  fontSize={12}
+                  fontWeight={400}
+                  onClick={() => {
+                    setIsEditingDescription(true);
+                    setIsEditingName(false);
+                  }}
+                >
+                  {workflow.description}
+                </Text>
+              )}
+            </VStack>
+          </HStack>
         </>
       }
     >
