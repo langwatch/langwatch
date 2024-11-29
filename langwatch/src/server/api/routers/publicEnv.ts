78e0a425cd65ab8a7fd25ad84cf4e76a0a7fc307
Richard Huth <richhuth@gmail.com>
2024-11-29 15:37:10 +0100

    Fix email alert ENV check

--- langwatch/src/server/api/routers/publicEnv.ts
+++ langwatch/src/server/api/routers/publicEnv.ts
@@ -16,7 +16,7 @@ export const publicEnvRouter = publicProcedure
       IS_ONPREM: env.IS_ONPREM,
       IS_QUICKWIT: env.IS_QUICKWIT,
       HAS_EMAIL_PROVIDER_KEY:
-        !!env.SENDGRID_API_KEY && !!(env.USE_AWS_SES && env.AWS_REGION),
+        !!env.SENDGRID_API_KEY || !!(env.USE_AWS_SES && env.AWS_REGION),
     };
 
     return publicEnvVars;
