7a08d92523a6bdd8578bb9b45f107c007314c370
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2024-11-30 16:21:17 +0100

    Exclude untracked files of course! This my settle the difference between local and CI/CD

--- infrastructure/scripts/get_langwatch_git_sha.sh
+++ infrastructure/scripts/get_langwatch_git_sha.sh
@@ -5,7 +5,7 @@ set -e
 cd ../langwatch
 
 salt="2024-11-28"
-files=$(git ls-files -o -c -m --exclude-standard | grep -v ^python-sdk | grep -v ^langwatch_nlp | grep -v ^typescript-sdk | xargs cat)
+files=$(git ls-files -c -m --exclude-standard | grep -v ^python-sdk | grep -v ^langwatch_nlp | grep -v ^typescript-sdk | xargs cat)
 current_hash=$(printf "%s%s" "$files" "$salt" | sha256sum | cut -d' ' -f1 | cut -c 1-7)
 
 echo "{\"tag\": \"hash-${current_hash}\", \"git_tag\": \"git-$(git rev-parse --short HEAD)\"}"
