c98e24f7d1b35adabb61b5a370d3be9cb1dd9681
Richard Huth <richhuth@gmail.com>
2025-01-21 11:40:11 +0100

    Remove test examples

--- python-sdk/examples/batch_evalutation.py
+++ python-sdk/examples/batch_evalutation.py
@@ -14,17 +14,13 @@ def callback(entry: DatasetEntry):
     # print(response)
     # return {"output": response}
 
-    return {
-        "output": "Hello! How can I help you today?",
-    }
+    return {"output": "Hello! How can I help you today?"}
 
 
 # Instantiate the BatchEvaluation object
 evaluation = BatchEvaluation(
-    dataset="test-custim",
-    evaluations=[
-        "test-eval",
-    ],
+    dataset="",  # Provide the actual dataset name here
+    evaluations=[""],  # Provide the actual evaluations here
     callback=callback,
 )
 
