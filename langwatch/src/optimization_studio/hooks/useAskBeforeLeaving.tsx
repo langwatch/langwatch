defdb3c4e4a60289a5c69f5854ea72e482364318
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2025-02-19 21:40:37 +0100

    Prevent the user to accidentally leave without saving

--- langwatch/src/optimization_studio/hooks/useAskBeforeLeaving.tsx
+++ langwatch/src/optimization_studio/hooks/useAskBeforeLeaving.tsx
@@ -3,32 +3,37 @@ import { useWorkflowStore } from "./useWorkflowStore";
 import { useEffect } from "react";
 
 export const useAskBeforeLeaving = () => {
-  const { evaluationStatus } = useWorkflowStore(
+  const { evaluationStatus, hasPendingChanges } = useWorkflowStore(
     useShallow((state) => {
       return {
         evaluationStatus: state.state.evaluation?.status,
+        hasPendingChanges: state.hasPendingChanges,
       };
     })
   );
 
   useEffect(() => {
-    let message = "";
-    if (evaluationStatus === "running" || evaluationStatus === "waiting") {
-      message = "An evaluation is running, are you sure you want to leave?";
-    }
-    if (!message) {
-      return;
-    }
+    const message =
+      evaluationStatus === "running" || evaluationStatus === "waiting"
+        ? "An evaluation is running, are you sure you want to leave?"
+        : undefined;
 
     const onBeforeUnload = (event: BeforeUnloadEvent) => {
-      event.preventDefault();
-      event.returnValue = message;
-      return message;
+      const message_ = message
+        ? message
+        : hasPendingChanges()
+        ? "Changes were not autosaved yet, are you sure you want to leave?"
+        : undefined;
+      if (message_) {
+        event.preventDefault();
+        event.returnValue = message;
+        return message;
+      }
     };
 
     window.addEventListener("beforeunload", onBeforeUnload);
     return () => {
       window.removeEventListener("beforeunload", onBeforeUnload);
     };
-  }, [evaluationStatus]);
+  }, [evaluationStatus, hasPendingChanges]);
 };
