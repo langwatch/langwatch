c58355c35fb96095f6b9d3cd16d8c27663845a2e
Richard Huth <richhuth@gmail.com>
2025-02-26 12:05:13 +0100

    Add type check, but allow for custom

--- langwatch/src/server/api/routers/evaluations.ts
+++ langwatch/src/server/api/routers/evaluations.ts
@@ -3,7 +3,10 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { TeamRoleGroup, checkUserPermissionForProject } from "../permission";
 import { evaluatorsSchema } from "../../evaluations/evaluators.zod.generated";
 import { runEvaluationForTrace } from "../../background/workers/evaluationsWorker";
-import { AVAILABLE_EVALUATORS } from "../../evaluations/evaluators.generated";
+import {
+  AVAILABLE_EVALUATORS,
+  type EvaluatorTypes,
+} from "../../evaluations/evaluators.generated";
 import { prisma } from "~/server/db";
 
 export const evaluationsRouter = createTRPCRouter({
@@ -51,7 +54,7 @@ export const evaluationsRouter = createTRPCRouter({
       const result = await runEvaluationForTrace({
         projectId: input.projectId,
         traceId: input.traceId,
-        evaluatorType: input.evaluatorType,
+        evaluatorType: input.evaluatorType as EvaluatorTypes,
         settings: input.settings,
         mappings: input.mappings ?? {},
       });
