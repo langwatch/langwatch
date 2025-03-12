068bb2fa1ddb1829ca774d9a089aed94c01e62bf
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2025-03-12 13:37:34 +0100

    Parse llm signature with demonstrations and format with black

--- langwatch_nlp/langwatch_nlp/studio/parser_v2.py
+++ langwatch_nlp/langwatch_nlp/studio/parser_v2.py
@@ -1,10 +1,13 @@
-from typing import Tuple
+from typing import Tuple, Type
 from langwatch_nlp.studio.modules.registry import PROMPTING_TECHNIQUES
 from langwatch_nlp.studio.types.dsl import Node, Workflow
 from langwatch_nlp.studio.dspy.workflow_module import WorkflowModule
 from jinja2 import Environment, FileSystemLoader
 import re
 import dspy
+
+from langwatch_nlp.studio.utils import transpose_inline_dataset_to_object_list
+import black
 
 env = Environment(
     loader=FileSystemLoader("./langwatch_nlp/studio/templates"),
@@ -14,21 +17,28 @@ env = Environment(
 env.keep_trailing_newline = True
 
 
-def parse_workflow(workflow: Workflow) -> WorkflowModule:
-    template = env.get_template("workflow.py.jinja")
-    module = template.render(workflow=workflow, debug_level=1)
-    module = re.sub(r"\n{3,}", "\n\n", module)
-    print("\n\nmodule", module, "\n\n")
+def render_template(template_name: str, format=False, **kwargs) -> str:
+    template = env.get_template(template_name)
+    code = template.render(**kwargs)
+    code = re.sub(r"\n{4,}", "\n\n", code)
+    if format:
+        code = black.format_str(code, mode=black.Mode())
+    return code
+
+
+def parse_workflow(workflow: Workflow, format=False) -> WorkflowModule:
+    module = render_template(
+        "workflow.py.jinja", format=format, workflow=workflow, debug_level=1
+    )
     namespace = {}
     exec(module, namespace)
     WorkflowModule = namespace["WorkflowModule"]
     return WorkflowModule
 
 
-def parse_component(node: Node, workflow: Workflow) -> Tuple[str, str]:
+def parse_component(node: Node, workflow: Workflow, format=False) -> Tuple[str, str]:
     match node.type:
         case "signature":
-            template = env.get_template("llm.py.jinja")
             parameters = {}
             if node.data.parameters:
                 for param in node.data.parameters:
@@ -55,8 +65,18 @@ def parse_component(node: Node, workflow: Workflow) -> Tuple[str, str]:
                 ),
                 None,
             )
+            demonstrations_dict = (
+                transpose_inline_dataset_to_object_list(demonstrations.value.inline)
+                if demonstrations
+                and demonstrations.value
+                and demonstrations.value.inline
+                else None
+            )
 
-            return f"{node.data.name}", template.render(
+            return f"{node.data.name}", render_template(
+                "llm.py.jinja",
+                format=format,
+                debug_level=1,
                 node_id=node.id,
                 component=node.data,
                 workflow=workflow,
@@ -65,18 +85,13 @@ def parse_component(node: Node, workflow: Workflow) -> Tuple[str, str]:
                     prompting_technique.value if prompting_technique else None
                 ),
                 llm_config=llm_config.value if llm_config else None,
-                demonstrations=demonstrations.value if demonstrations else None,
+                demonstrations=demonstrations_dict,
             )
         case _:
             return "None", ""
 
 
-def parse_and_instantiate_component(node: Node, workflow: Workflow) -> dspy.Module:
-    class_name, component_code = parse_component(node, workflow)
-    return instantiate_component(component_code, class_name)
-
-
-def instantiate_component(component_code: str, class_name: str) -> dspy.Module:
+def get_component_class(component_code: str, class_name: str) -> Type[dspy.Module]:
     namespace = {}
     exec(component_code, namespace)
     return namespace[class_name]
