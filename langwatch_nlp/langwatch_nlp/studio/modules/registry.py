9247f6611303f844a6d099f8170718428435c190
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2025-03-13 14:18:05 +0100

    Add evaluators instantiation on the workflow

--- langwatch_nlp/langwatch_nlp/studio/modules/registry.py
+++ langwatch_nlp/langwatch_nlp/studio/modules/registry.py
@@ -13,12 +13,28 @@ from dspy.teleprompt import (
 
 import dspy
 
+from langwatch_nlp.studio.types.dsl import FieldType
+
 EVALUATORS: dict[str, Type[dspy.Module]] = {
     "ExactMatchEvaluator": ExactMatchEvaluator,
     "AnswerCorrectnessEvaluator": AnswerCorrectnessEvaluator,
     "LangWatchEvaluator": LangWatchEvaluator,
 }
 
+EVALUATORS_FOR_TEMPLATE = {
+    "ExactMatchEvaluator": {
+        "import": "from langwatch_nlp.studio.modules.evaluators.exact_match import ExactMatchEvaluator",
+        "class": "ExactMatchEvaluator",
+    },
+    "AnswerCorrectnessEvaluator": {
+        "import": "from langwatch_nlp.studio.modules.evaluators.answer_correctness import AnswerCorrectnessEvaluator",
+        "class": "AnswerCorrectnessEvaluator",
+    },
+    "LangWatchEvaluator": {
+        "import": "from langwatch_nlp.studio.modules.evaluators.langwatch import LangWatchEvaluator",
+        "class": "LangWatchEvaluator",
+    },
+}
 
 PromptingTechniqueTypes = Union[Type[dspy.ChainOfThought], Type[dspy.ReAct]]
 
@@ -26,9 +42,27 @@ PROMPTING_TECHNIQUES: dict[str, PromptingTechniqueTypes] = {
     "ChainOfThought": dspy.ChainOfThought,
 }
 
+PROMPTING_TECHNIQUES_FOR_TEMPLATE: dict[str, dict[str, str]] = {
+    "ChainOfThought": {
+        "import": "import dspy",
+        "class": "dspy.ChainOfThought",
+    },
+}
+
 RETRIEVERS: dict[str, Type[dspy.Retrieve]] = {
     "ColBERTv2": ColBERTv2RM,
     "WeaviateRM": WeaviateRMWithConnection,
+}
+
+RETRIEVERS_FOR_TEMPLATE: dict[str, dict[str, str]] = {
+    "ColBERTv2": {
+        "import": "from langwatch_nlp.studio.dspy.retrieve import ColBERTv2RM",
+        "class": "ColBERTv2RM",
+    },
+    "WeaviateRM": {
+        "import": "from langwatch_nlp.studio.dspy.retrieve import WeaviateRMWithConnection",
+        "class": "WeaviateRMWithConnection",
+    },
 }
 
 OPTIMIZERS: dict[
@@ -38,4 +72,13 @@ OPTIMIZERS: dict[
     "MIPROv2ZeroShot": MIPROv2,
     "MIPROv2": MIPROv2,
     "BootstrapFewShotWithRandomSearch": BootstrapFewShotWithRandomSearch,
+}
+
+FIELD_TYPE_TO_DSPY_TYPE = {
+    FieldType.image: "dspy.Image",
+    FieldType.str: "str",
+    FieldType.int: "int",
+    FieldType.float: "float",
+    FieldType.bool: "bool",
+    FieldType.list_str: "list[str]",
 }
