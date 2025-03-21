5d8d421493252349f065d643016ee8f17de78687
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2025-03-21 11:44:50 +0100

    Change step buttong to also be a radio card

--- langwatch/src/components/evaluations/wizard/steps/TaskSelection.tsx
+++ langwatch/src/components/evaluations/wizard/steps/TaskSelection.tsx
@@ -1,12 +1,22 @@
-import { Heading, Text, VStack } from "@chakra-ui/react";
+import { Heading, RadioCard, Text, VStack } from "@chakra-ui/react";
 import { Activity, Edit3 } from "react-feather";
 import { LuBadgeCheck, LuListChecks, LuShield } from "react-icons/lu";
+import {
+  useEvaluationWizardStore,
+  type State,
+} from "~/hooks/useEvaluationWizardStore";
 import { ColorfulBlockIcon } from "../../../../optimization_studio/components/ColorfulBlockIcons";
 import { StepButton } from "../../StepButton";
-import { useEvaluationWizardStore } from "~/hooks/useEvaluationWizardStore";
 
 export function TaskSelection() {
-  const { setWizardState } = useEvaluationWizardStore();
+  const { wizardState, setWizardState } = useEvaluationWizardStore();
+
+  const handleTaskSelection = (task: State["wizardState"]["task"]) => {
+    setWizardState({
+      step: "dataset",
+      task,
+    });
+  };
 
   return (
     <>
@@ -16,73 +26,98 @@ export function TaskSelection() {
         </Heading>
         <Text>Select what evaluation flow you want to follow</Text>
       </VStack>
-      <VStack width="full" gap={3}>
-        <StepButton
-          title="Set up real-time evaluation"
-          description="Evaluate messages as they arrive in production"
-          onClick={() => setWizardState({ step: "dataset", task: "real_time" })}
-          icon={
-            <ColorfulBlockIcon
-              color="green.400"
-              size="md"
-              icon={<Activity />}
-              marginTop="-2px"
-            />
-          }
-        />
-        <StepButton
-          title="Evaluate your LLM pipeline"
-          description="Run a batch evaluation of dataset examples against your existing LLM application"
-          icon={
-            <ColorfulBlockIcon
-              color="blue.400"
-              size="md"
-              icon={<LuListChecks />}
-              marginTop="-2px"
-            />
-          }
-          disabled
-        />
-        <StepButton
-          title="Prompt Creation"
-          description="Build a new prompt and evaluate the quality of the outputs, iteratively improving it"
-          icon={
-            <ColorfulBlockIcon
-              color="purple.400"
-              size="md"
-              icon={<Edit3 />}
-              marginTop="-2px"
-            />
-          }
-          disabled
-        />
-        <StepButton
-          title="Create Custom Evaluator"
-          description="Build your own reliable evaluator to be used by other flows, measuring and ensuring its accuracy"
-          icon={
-            <ColorfulBlockIcon
-              color="orange.400"
-              size="md"
-              icon={<LuBadgeCheck />}
-              marginTop="-2px"
-            />
-          }
-          disabled
-        />
-        <StepButton
-          title="Scan for Vulnerabilities (Coming Soon)"
-          description="Run malicious datasets and adversarial attacks against your LLM application for Red Teaming"
-          icon={
-            <ColorfulBlockIcon
-              color="teal.400"
-              size="md"
-              icon={<LuShield />}
-              marginTop="-2px"
-            />
-          }
-          disabled
-        />
-      </VStack>
+      <RadioCard.Root
+        value={wizardState.task}
+        onValueChange={(e) =>
+          handleTaskSelection(e.value as State["wizardState"]["task"])
+        }
+      >
+        <VStack width="full" gap={3}>
+          <StepButton
+            colorPalette="green"
+            value="real_time"
+            title="Set up real-time evaluation"
+            description="Evaluate messages as they arrive in production"
+            onClick={() => handleTaskSelection("real_time")}
+            icon={
+              <ColorfulBlockIcon
+                color="green.400"
+                size="md"
+                icon={<Activity />}
+                marginTop="-2px"
+              />
+            }
+          />
+          <StepButton
+            colorPalette="blue"
+            value="batch"
+            title="Evaluate your LLM pipeline"
+            description="Run a batch evaluation of dataset examples against your existing LLM application"
+            // Disabled for now
+            // onClick={() => handleTaskSelection("batch")}
+            disabled
+            icon={
+              <ColorfulBlockIcon
+                color="blue.400"
+                size="md"
+                icon={<LuListChecks />}
+                marginTop="-2px"
+              />
+            }
+          />
+          <StepButton
+            colorPalette="purple"
+            value="prompt_creation"
+            title="Prompt Creation"
+            description="Build a new prompt and evaluate the quality of the outputs, iteratively improving it"
+            // Disabled for now
+            // onClick={() => handleTaskSelection("prompt_creation")}
+            disabled
+            icon={
+              <ColorfulBlockIcon
+                color="purple.400"
+                size="md"
+                icon={<Edit3 />}
+                marginTop="-2px"
+              />
+            }
+          />
+          <StepButton
+            colorPalette="orange"
+            value="custom_evaluator"
+            title="Create Custom Evaluator"
+            description="Build your own reliable evaluator to be used by other flows, measuring and ensuring its accuracy"
+            // Disabled for now
+            // onClick={() => handleTaskSelection("custom_evaluator")}
+            disabled
+            icon={
+              <ColorfulBlockIcon
+                color="orange.400"
+                size="md"
+                icon={<LuBadgeCheck />}
+                marginTop="-2px"
+              />
+            }
+          />
+          <StepButton
+            colorPalette="teal"
+            value="scan"
+            title="Scan for Vulnerabilities (Coming Soon)"
+            description="Run malicious datasets and adversarial attacks against your LLM application for Red Teaming"
+            // Disabled for now
+            // onClick={() => handleTaskSelection("scan")}
+            disabled
+            icon={
+              <ColorfulBlockIcon
+                color="teal.400"
+                size="md"
+                icon={<LuShield />}
+                marginTop="-2px"
+              />
+            }
+          />
+        </VStack>
+      </RadioCard.Root>
     </>
   );
 }
