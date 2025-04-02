8bf25e3a04acbc6b170321ce16ae50fc5582c461
Richard Huth <richhuth@gmail.com>
2025-04-02 16:21:55 +0200

    Remove console

--- langwatch/src/server/auth.ts
+++ langwatch/src/server/auth.ts
@@ -93,9 +93,6 @@ export const authOptions = (
         where: { id: dbSession?.userId },
       });
 
-      console.log("dbUser", dbUser);
-      console.log("user", user);
-
       if (dbUser?.email !== user.email) {
         throw new Error("DIFFERENT_EMAIL_NOT_ALLOWED");
       }
