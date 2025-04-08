00a07727ffbfa2235f5e702e7efd801bf7e69304
Richard Huth <richhuth@gmail.com>
2025-04-08 12:09:55 +0200

    Remove non relevant sql

--- langwatch/prisma/migrations/20250403125535_add_elasticsearch_hybrid/migration.sql
+++ langwatch/prisma/migrations/20250403125535_add_elasticsearch_hybrid/migration.sql
@@ -1,16 +1,5 @@
 -- AlterTable
-ALTER TABLE "Monitor" RENAME CONSTRAINT "Check_pkey" TO "Monitor_pkey";
-
--- AlterTable
 ALTER TABLE "Organization" ADD COLUMN     "elasticsearchApiKey" TEXT,
 ADD COLUMN     "elasticsearchNodeUrl" TEXT,
 ADD COLUMN     "useCustomElasticsearch" BOOLEAN NOT NULL DEFAULT false;
 
--- RenameIndex
-ALTER INDEX "Check_experimentId_key" RENAME TO "Monitor_experimentId_key";
-
--- RenameIndex
-ALTER INDEX "Check_projectId_idx" RENAME TO "Monitor_projectId_idx";
-
--- RenameIndex
-ALTER INDEX "Check_projectId_slug_key" RENAME TO "Monitor_projectId_slug_key";
