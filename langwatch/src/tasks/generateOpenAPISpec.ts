b35cb3605d01aa14eeee7d3a69eee8b45562f518
drewdrew <drewdrewthis@gmail.com>
2025-04-15 18:19:45 +0100

    Generate open api specs

--- langwatch/src/tasks/generateOpenAPISpec.ts
+++ langwatch/src/tasks/generateOpenAPISpec.ts
@@ -1,13 +1,24 @@
 import { generateSpecs } from "hono-openapi";
 
 import { app as datasetApp } from "../app/api/dataset/[[...route]]/app";
-
+import { app as llmConfigsApp } from "../app/api/llmConfigs/[[...route]]/app";
 /**
  * This task generates the OpenAPI spec for the dataset API.
  * It is very bare bones right now just so we copy and paste into langwatch-docs, in
  * the future it will evolve to generate the whole spec file directly for all endpoints
  */
 export default async function execute() {
-  const openAPISpec = await generateSpecs(datasetApp);
-  console.log(JSON.stringify(openAPISpec, null, 2), {});
+  const datasetSpec = await generateSpecs(datasetApp);
+  const llmConfigsSpec = await generateSpecs(llmConfigsApp);
+
+  console.log(
+    JSON.stringify(
+      {
+        ...datasetSpec,
+        ...llmConfigsSpec,
+      },
+      null,
+      2
+    )
+  );
 }
