4b652b6be4b7e7805acf035d29c058aeca3b4a9a
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2025-04-17 13:40:56 +0200

    PR comments, better code organization

--- langwatch/src/components/MainMenu.tsx
+++ langwatch/src/components/MainMenu.tsx
@@ -11,15 +11,12 @@ import type { Project } from "@prisma/client";
 import { useRouter } from "next/router";
 import React, { useMemo } from "react";
 import {
-  Bell,
   BookOpen,
   CheckSquare,
   Edit,
   GitHub,
-  Layers,
   MessageSquare,
   Settings,
-  Shield,
   Table,
   TrendingUp,
 } from "react-feather";
@@ -38,205 +35,201 @@ import { useColorRawValue } from "./ui/color-mode";
 import { Link } from "./ui/link";
 import { Tooltip } from "./ui/tooltip";
 
-export const MainMenu = React.memo(
-  function MainMenu({ menuWidth }: { menuWidth: number }) {
-    const { project, hasOrganizationPermission, isPublicRoute } =
-      useOrganizationTeamProject();
-    const { assignedQueueItems, memberAccessibleQueueItems } =
-      useAnnotationQueues();
-    const totalQueueItems = useMemo(
-      () =>
-        (assignedQueueItems?.filter((item) => !item.doneAt)?.length ?? 0) +
-        (memberAccessibleQueueItems?.filter((item) => !item.doneAt)?.length ??
-          0),
-      [assignedQueueItems, memberAccessibleQueueItems]
-    );
+export const MENU_WIDTH = "88px";
 
-    return (
-      <Box
-        borderRightWidth="1px"
-        borderRightColor="gray.300"
-        background="white"
-        transition="all 0.2s ease-in-out"
-        width={menuWidth + "px"}
-        minWidth={menuWidth + "px"}
-        overflowX="hidden"
+export const MainMenu = React.memo(function MainMenu() {
+  const { project, hasOrganizationPermission, isPublicRoute } =
+    useOrganizationTeamProject();
+  const { assignedQueueItems, memberAccessibleQueueItems } =
+    useAnnotationQueues();
+  const totalQueueItems = useMemo(
+    () =>
+      (assignedQueueItems?.filter((item) => !item.doneAt)?.length ?? 0) +
+      (memberAccessibleQueueItems?.filter((item) => !item.doneAt)?.length ?? 0),
+    [assignedQueueItems, memberAccessibleQueueItems]
+  );
+
+  return (
+    <Box
+      borderRightWidth="1px"
+      borderRightColor="gray.300"
+      background="white"
+      transition="all 0.2s ease-in-out"
+      width={MENU_WIDTH}
+      minWidth={MENU_WIDTH}
+      overflowX="hidden"
+      height="100vh"
+      position="sticky"
+      top={0}
+    >
+      <VStack
+        paddingX={0}
+        paddingTop={4}
+        paddingBottom={0}
+        gap={4}
         height="100vh"
-        position="sticky"
-        top={0}
+        align="start"
+        width="full"
       >
-        <VStack
-          paddingX={0}
-          paddingTop={4}
-          paddingBottom={0}
-          gap={4}
-          height="100vh"
-          align="start"
-          width="full"
-        >
-          <Center width="full" paddingX="6px">
-            <LogoIcon width={25} height={34} />
-          </Center>
+        <Center width="full" paddingX="6px">
+          <LogoIcon width={25} height={34} />
+        </Center>
 
-          <VStack width="full" height="full" gap={0} align="start">
-            <PageMenuLink
-              path={projectRoutes.home.path}
-              icon={TrendingUp}
-              label={projectRoutes.home.title}
-              project={project}
-            />
-            <PageMenuLink
-              path={projectRoutes.messages.path}
-              icon={MessageSquare}
-              label={projectRoutes.messages.title}
-              project={project}
-            />
+        <VStack width="full" height="full" gap={0} align="start">
+          <PageMenuLink
+            path={projectRoutes.home.path}
+            icon={TrendingUp}
+            label={projectRoutes.home.title}
+            project={project}
+          />
+          <PageMenuLink
+            path={projectRoutes.messages.path}
+            icon={MessageSquare}
+            label={projectRoutes.messages.title}
+            project={project}
+          />
 
-            <PageMenuLink
-              path={projectRoutes.evaluations.path}
-              icon={CheckSquare}
-              label={projectRoutes.evaluations.title}
-              project={project}
-            />
+          <PageMenuLink
+            path={projectRoutes.evaluations.path}
+            icon={CheckSquare}
+            label={projectRoutes.evaluations.title}
+            project={project}
+          />
 
-            <PageMenuLink
-              path={projectRoutes.workflows.path}
-              icon={PuzzleIcon}
-              label={projectRoutes.workflows.title}
-              project={project}
-            />
+          <PageMenuLink
+            path={projectRoutes.workflows.path}
+            icon={PuzzleIcon}
+            label={projectRoutes.workflows.title}
+            project={project}
+          />
 
-            {/* <PageMenuLink
+          {/* <PageMenuLink
               path={projectRoutes.promptConfigs.path}
               icon={Layers} // Use an appropriate icon
               label={projectRoutes.promptConfigs.title}
               project={project}
             /> */}
 
+          <PageMenuLink
+            path={projectRoutes.datasets.path}
+            icon={Table}
+            label={projectRoutes.datasets.title}
+            project={project}
+          />
+          <PageMenuLink
+            path={projectRoutes.annotations.path}
+            icon={Edit}
+            label={projectRoutes.annotations.title}
+            project={project}
+            badgeNumber={totalQueueItems}
+            iconStyle={{ marginLeft: "1px" }}
+          />
+
+          {(!!hasOrganizationPermission(
+            OrganizationRoleGroup.ORGANIZATION_VIEW
+          ) ||
+            isPublicRoute) && (
             <PageMenuLink
-              path={projectRoutes.datasets.path}
-              icon={Table}
-              label={projectRoutes.datasets.title}
+              path={projectRoutes.settings.path}
+              icon={Settings}
+              label={projectRoutes.settings.title}
               project={project}
             />
-            <PageMenuLink
-              path={projectRoutes.annotations.path}
-              icon={Edit}
-              label={projectRoutes.annotations.title}
-              project={project}
-              badgeNumber={totalQueueItems}
-              iconStyle={{ marginLeft: "1px" }}
-            />
+          )}
 
-            {(!!hasOrganizationPermission(
-              OrganizationRoleGroup.ORGANIZATION_VIEW
-            ) ||
-              isPublicRoute) && (
-              <PageMenuLink
-                path={projectRoutes.settings.path}
-                icon={Settings}
-                label={projectRoutes.settings.title}
-                project={project}
-              />
-            )}
+          <Spacer />
 
-            <Spacer />
-
-            {(window as any)?.$crisp && project && (
-              <SideMenuLink
-                size="sm"
-                href="#"
-                onClick={(e) => {
-                  e.preventDefault();
-                  (window as any)?.$crisp.push(["do", "chat:show"]);
-                  (window as any)?.$crisp.push(["do", "chat:toggle"]);
-                }}
-                icon={
+          {(window as any)?.$crisp && project && (
+            <SideMenuLink
+              size="sm"
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                (window as any)?.$crisp.push(["do", "chat:show"]);
+                (window as any)?.$crisp.push(["do", "chat:toggle"]);
+              }}
+              icon={
+                <Box
+                  position="relative"
+                  color="white"
+                  padding={2}
+                  marginX={-2}
+                  borderRadius="full"
+                  minWidth={0}
+                  minHeight={0}
+                  backgroundColor="blue.500"
+                  transition="all 0.2s ease-in-out"
+                  _groupHover={{
+                    transform: "scale(1.2)",
+                  }}
+                  _active={{
+                    color: "white",
+                  }}
+                >
+                  <ChatBalloonIcon width={20} height={20} />
                   <Box
-                    position="relative"
-                    color="white"
-                    padding={2}
-                    marginX={-2}
+                    position="absolute"
+                    bottom="0px"
+                    right="0px"
+                    width="10px"
+                    height="10px"
                     borderRadius="full"
-                    minWidth={0}
-                    minHeight={0}
-                    backgroundColor="blue.500"
-                    transition="all 0.2s ease-in-out"
-                    _groupHover={{
-                      transform: "scale(1.2)",
-                    }}
-                    _active={{
-                      color: "white",
-                    }}
-                  >
-                    <ChatBalloonIcon width={20} height={20} />
-                    <Box
-                      position="absolute"
-                      bottom="0px"
-                      right="0px"
-                      width="10px"
-                      height="10px"
-                      borderRadius="full"
-                      backgroundColor="green.500"
-                      border="1px solid"
-                      borderColor="white"
-                    />
-                  </Box>
-                }
-                label="Live Help"
-                isActive={false}
-                project={project}
-              />
-            )}
-            <HStack width="full" gap={0} paddingX={2} paddingTop={2}>
-              <SideMenuLink
-                size="sm"
-                href="https://docs.langwatch.ai"
-                icon={
-                  <IconWrapper width="14px" height="14px" marginLeft="1px">
-                    <BookOpen />
-                  </IconWrapper>
-                }
-                label="Documentation"
-                isActive={false}
-                project={project}
-              />
+                    backgroundColor="green.500"
+                    border="1px solid"
+                    borderColor="white"
+                  />
+                </Box>
+              }
+              label="Live Help"
+              isActive={false}
+              project={project}
+            />
+          )}
+          <HStack width="full" gap={0} paddingX={2} paddingTop={2}>
+            <SideMenuLink
+              size="sm"
+              href="https://docs.langwatch.ai"
+              icon={
+                <IconWrapper width="14px" height="14px" marginLeft="1px">
+                  <BookOpen />
+                </IconWrapper>
+              }
+              label="Documentation"
+              isActive={false}
+              project={project}
+            />
 
-              <SideMenuLink
-                size="sm"
-                href="https://github.com/langwatch/langwatch"
-                icon={
-                  <IconWrapper width="14px" height="14px">
-                    <GitHub />
-                  </IconWrapper>
-                }
-                label="GitHub"
-                isActive={false}
-                project={project}
-              />
+            <SideMenuLink
+              size="sm"
+              href="https://github.com/langwatch/langwatch"
+              icon={
+                <IconWrapper width="14px" height="14px">
+                  <GitHub />
+                </IconWrapper>
+              }
+              label="GitHub"
+              isActive={false}
+              project={project}
+            />
 
-              <SideMenuLink
-                size="sm"
-                href="https://discord.gg/kT4PhDS2gH"
-                icon={
-                  <IconWrapper width="14px" height="14px">
-                    <DiscordOutlineIcon />
-                  </IconWrapper>
-                }
-                label="Community"
-                isActive={false}
-                project={project}
-              />
-            </HStack>
-          </VStack>
+            <SideMenuLink
+              size="sm"
+              href="https://discord.gg/kT4PhDS2gH"
+              icon={
+                <IconWrapper width="14px" height="14px">
+                  <DiscordOutlineIcon />
+                </IconWrapper>
+              }
+              label="Community"
+              isActive={false}
+              project={project}
+            />
+          </HStack>
         </VStack>
-      </Box>
-    );
-  },
-  (prevProps, nextProps) => {
-    return prevProps.menuWidth === nextProps.menuWidth;
-  }
-);
+      </VStack>
+    </Box>
+  );
+});
 
 const PageMenuLink = ({
   icon,
@@ -362,8 +355,8 @@ const SideMenuLink = ({
       <Link
         variant="plain"
         width="full"
-        paddingX={size === "sm" ? 0 : 4}
-        paddingY={size === "sm" ? 2 : 3}
+        paddingX={4}
+        paddingY={3}
         href={href}
         aria-label={label}
         onClick={(e) => {
@@ -373,10 +366,15 @@ const SideMenuLink = ({
           });
           onClick?.(e);
         }}
-        fontSize={size === "sm" ? 10 : 11}
+        fontSize={11}
         _hover={{
           backgroundColor: "gray.50",
         }}
+        {...(size === "sm" && {
+          paddingX: 0,
+          paddingY: 2,
+          fontSize: 10,
+        })}
       >
         <VStack width="full" align="center" gap={1} minHeight="21px">
           <VStack align="start" position="relative">
@@ -388,7 +386,6 @@ const SideMenuLink = ({
               </Box>
             )}
           </VStack>
-          {/* {isHovered && ( */}
           {size === "md" && (
             <Text color={isActive ? "orange.600" : "gray.600"}>{label}</Text>
           )}
