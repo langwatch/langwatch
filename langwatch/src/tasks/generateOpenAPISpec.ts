31bd3da8ae0764a46c4a4c96dfa5f81993d595ad
drewdrew <drewdrewthis@gmail.com>
2025-04-17 09:25:31 +0100

    Add current spec

--- langwatch/src/tasks/generateOpenAPISpec.ts
+++ langwatch/src/tasks/generateOpenAPISpec.ts
@@ -1,7 +1,8 @@
 import { generateSpecs } from "hono-openapi";
-
 import { app as datasetApp } from "../app/api/dataset/[[...route]]/app";
 import { app as llmConfigsApp } from "../app/api/llmConfigs/[[...route]]/app";
+import deepmerge from "deepmerge";
+
 /**
  * This task generates the OpenAPI spec for the dataset API.
  * It is very bare bones right now just so we copy and paste into langwatch-docs, in
@@ -10,31 +11,7 @@ import { app as llmConfigsApp } from "../app/api/llmConfigs/[[...route]]/app";
 export default async function execute() {
   const datasetSpec = await generateSpecs(datasetApp);
   const llmConfigsSpec = await generateSpecs(llmConfigsApp);
+  const mergedSpec = deepmerge(datasetSpec, llmConfigsSpec);
 
-  console.log(
-    JSON.stringify(
-      {
-        ...datasetSpec,
-        paths: {
-          ...datasetSpec.paths,
-          ...llmConfigsSpec.paths,
-        },
-        components: {
-          ...datasetSpec.components,
-          ...llmConfigsSpec.components,
-          schemas: {
-            ...(datasetSpec.components?.schemas || {}),
-            ...(llmConfigsSpec.components?.schemas || {}),
-          },
-          securitySchemes: {
-            ...(datasetSpec.components?.securitySchemes || {}),
-            ...(llmConfigsSpec.components?.securitySchemes || {}),
-          },
-        },
-        tags: [...(datasetSpec.tags || []), ...(llmConfigsSpec.tags || [])],
-      },
-      null,
-      2
-    )
-  );
+  console.log(JSON.stringify(mergedSpec, null, 2));
 }
