d1619cc7629db8209cd25d89811e0f704165699e
Alexander Forbes-Reed <git@alex.forbes.red>
2025-04-29 15:26:40 +0200

    added searching traces

--- langwatch/src/server/elasticsearch/traces.ts
+++ langwatch/src/server/elasticsearch/traces.ts
@@ -1,3 +1,12 @@
+import * as T from '@elastic/elasticsearch/lib/api/types'
+import { Client as ElasticClient } from "@elastic/elasticsearch";
+import {
+  type ElasticSearchTrace,
+  type Trace,
+} from "../tracer/types";
+import { TRACE_INDEX, esClient } from "../elasticsearch";
+import type { Protections } from "./protections";
+import { transformElasticSearchTraceToTrace } from "./transformers";
 interface ProjectConnectionConfig {
   projectId: string;
 }
@@ -9,3 +18,36 @@ interface TestConnectionConfig {
 }
 
 type ConnectionConfig = ProjectConnectionConfig | OrganizationConnectionConfig | TestConnectionConfig;
+
+interface SearchTracesOptions {
+  connConfig: ConnectionConfig;
+  search: Parameters<ElasticClient['search']>[0] & {
+    index?: typeof TRACE_INDEX[keyof typeof TRACE_INDEX];
+    size?: number;
+  };
+  protections: Protections;
+}
+
+export async function searchTraces({
+  connConfig,
+  search: {
+    index = TRACE_INDEX.alias,
+    size = 10,
+    ...searchParams
+  },
+  protections = {},
+}: SearchTracesOptions): Promise<Trace[]> {
+  const client = await esClient(connConfig);
+  const result = await client.search<ElasticSearchTrace>({
+    index,
+    size,
+    ...searchParams
+  });
+
+  const traces = result.hits.hits
+    .map((hit) => hit._source!)
+    .filter((x) => x)
+    .map((t) => transformElasticSearchTraceToTrace(t, protections));
+
+  return traces;
+}
