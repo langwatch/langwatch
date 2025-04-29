315d954b4b1716dd51954c5751e498613059d348
Alexander Forbes-Reed <git@alex.forbes.red>
2025-04-29 10:08:58 +0200

    fix migration

--- langwatch/prisma/migrations/20250428100116_add_redaction_for_inputs_outputs_to_the_project/migration.sql
+++ langwatch/prisma/migrations/20250428100116_add_redaction_for_inputs_outputs_to_the_project/migration.sql
@@ -4,12 +4,3 @@ CREATE TYPE "ProjectSensitiveDataVisibilityLevel" AS ENUM ('REDACTED_TO_ALL', 'V
 -- AlterTable
 ALTER TABLE "Project" ADD COLUMN     "capturedInputVisibility" "ProjectSensitiveDataVisibilityLevel" NOT NULL DEFAULT 'VISIBLE_TO_ALL',
 ADD COLUMN     "capturedOutputVisibility" "ProjectSensitiveDataVisibilityLevel" NOT NULL DEFAULT 'VISIBLE_TO_ALL';
-
--- RenameIndex
-ALTER INDEX "Check_experimentId_key" RENAME TO "Monitor_experimentId_key";
-
--- RenameIndex
-ALTER INDEX "Check_projectId_idx" RENAME TO "Monitor_projectId_idx";
-
--- RenameIndex
-ALTER INDEX "Check_projectId_slug_key" RENAME TO "Monitor_projectId_slug_key";
