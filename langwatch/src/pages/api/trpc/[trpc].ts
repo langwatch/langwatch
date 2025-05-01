7fdc4527490a462d9e792c1b05bd91c9eca3fa8c
Alexander Forbes-Reed <git@alex.forbes.red>
2025-05-02 00:04:45 +0200

    use it here too

--- langwatch/src/pages/api/trpc/[trpc].ts
+++ langwatch/src/pages/api/trpc/[trpc].ts
@@ -3,8 +3,24 @@ import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
+import { createLogger } from "~/utils/logger";
+
+const logger = createLogger("langwatch:trpc");
+
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
+  onError: (({ ctx, error, input, path, type }) => {
+    const logData: Record<string, any> = {
+      error,
+      path,
+      type,
+      userId: (ctx?.session?.user?.id) || null,
+      projectId: (input as any)?.projectId,
+      organizationId: (input as any)?.organizationId,
+    };
+
+    return logger.error(logData, "trpc error");
+  }),
 });
