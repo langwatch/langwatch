87e86ffad41522e5d6873e0f3fff9bca588bb66a
Alexander Forbes-Reed <git@alex.forbes.red>
2025-05-09 18:04:49 +0200

    added permission

--- langwatch/src/server/api/permission.ts
+++ langwatch/src/server/api/permission.ts
@@ -34,6 +34,7 @@ export const teamRolePermissionMapping = {
   EXPERIMENTS_MANAGE: [TeamUserRole.ADMIN, TeamUserRole.MEMBER],
   DATASETS_VIEW: [TeamUserRole.ADMIN, TeamUserRole.MEMBER],
   DATASETS_MANAGE: [TeamUserRole.ADMIN, TeamUserRole.MEMBER],
+  COPILOT: [TeamUserRole.ADMIN, TeamUserRole.MEMBER],
   ANNOTATIONS_MANAGE: [TeamUserRole.ADMIN, TeamUserRole.MEMBER],
   TRIGGERS_MANAGE: [TeamUserRole.ADMIN, TeamUserRole.MEMBER],
   PLAYGROUND: [TeamUserRole.ADMIN, TeamUserRole.MEMBER],
