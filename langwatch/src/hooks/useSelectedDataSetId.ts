ea04940d24b6f7c27eff015489a20cdd35d84b23
drewdrew <drewdrewthis@gmail.com>
2025-05-14 16:21:14 +0200

    Update doc

--- langwatch/src/hooks/useSelectedDataSetId.ts
+++ langwatch/src/hooks/useSelectedDataSetId.ts
@@ -1,5 +1,9 @@
 import { useLocalStorage } from "usehooks-ts";
 
+/**
+ * This hook is used to store the selected dataset id in the local storage.
+ * @returns The selected dataset id and a function to set the selected dataset id.
+ */
 export const useSelectedDataSetId = () => {
   const [selectedDataSetId, setSelectedDataSetId] = useLocalStorage<string>(
     "selectedDatasetId",
