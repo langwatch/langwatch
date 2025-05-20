ac55b4e2209482704d8b631eb3a68bed26b2c184
drewdrew <drewdrewthis@gmail.com>
2025-05-20 09:29:00 +0200

    Use internal package

--- python-sdk/examples/openai_bot_prompt.py
+++ python-sdk/examples/openai_bot_prompt.py
@@ -20,12 +20,11 @@ async def main():
     ## directly: 
     # Raw prompt {model: 'gpt-4o-mini', messages: [{ role: 'system', content: 'The user is {user_name} and their email is {user_email}' }]}
     # Autobuilt prompt { model: 'gpt-4o-mini', messages: [{ role: 'system', content: 'The user is John Doe and their email is john.doe@example.com' }]}
-    prompt = langwatch.prompts.get_prompt('prompt_GjrWUJ7TIpJ71QGXd-B5M', input="I like to eat pizza")
+    prompt = langwatch.prompts.get_prompt('prompt_GjrWUJ7TIpJ71QGXd-B5M')
+    messages = prompt.format_messages(input="I like to eat pizza")
 
     print(prompt)
-
-    model = prompt["model"].split("openai/")[1]
-    completion = client.chat.completions.create(model=model, messages=prompt["messages"])
+    completion = client.chat.completions.create(model=prompt.model, messages=messages)
     print(completion)
 
 if __name__ == "__main__":
