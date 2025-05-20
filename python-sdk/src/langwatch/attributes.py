7765a1dd33c5d526eec8132d0f9f97f68044059c
drewdrew <drewdrewthis@gmail.com>
2025-05-20 10:08:21 +0200

    Add event tracing

--- python-sdk/src/langwatch/attributes.py
+++ python-sdk/src/langwatch/attributes.py
@@ -69,6 +69,7 @@ class AttributeKey:
     LangWatchThreadId = "langwatch.thread.id"
     LangWatchSessionId = "langwatch.session.id" # We use thread_id for session_id
     LangWatchEventEvaluationCustom = "langwatch.evaluation.custom"
+    LangWatchEventFetchPrompt = "langwatch.event.fetch_prompt"
 
     LangWatchPromptId = "langwatch.prompt.id"
     LangWatchPromptVersionId = "langwatch.prompt.version.id"
