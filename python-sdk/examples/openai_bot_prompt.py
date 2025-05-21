b84b43a454da5a21183b49e0a4a839d0b4c9c9b6
drewdrew <drewdrewthis@gmail.com>
2025-05-21 16:50:47 +0200

    Add correct matching for prompt ids in metadata

--- python-sdk/examples/openai_bot_prompt.py
+++ python-sdk/examples/openai_bot_prompt.py
@@ -22,7 +22,7 @@ async def main():
     ## directly:
     # Raw prompt {model: 'gpt-4o-mini', messages: [{ role: 'system', content: 'The user is {user_name} and their email is {user_email}' }]}
     # Autobuilt prompt { model: 'gpt-4o-mini', messages: [{ role: 'system', content: 'The user is John Doe and their email is john.doe@example.com' }]}
-    prompt_id = "prompt_jTJP5ob_8zlTp7Jw0kNac"
+    prompt_id = "prompt_a6EDs9WsgnZuq9KnVbOXc"
     prompt = langwatch.prompts.get_prompt(prompt_id)
     print(prompt.raw_config())
     messages = prompt.format_messages(input="I like to eat pizza")
