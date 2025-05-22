3b158771a2f5ddc3f463c42bc5ff3d176cba9942
drewdrew <drewdrewthis@gmail.com>
2025-05-22 14:27:04 +0200

    Fix migration

--- langwatch/elastic/migrations/202505220000_set_type_for_prompt_metadata.ts
+++ langwatch/elastic/migrations/202505220000_set_type_for_prompt_metadata.ts
@@ -1,14 +1,23 @@
-import type { MappingProperty } from "@elastic/elasticsearch/lib/api/types";
-import { DSPY_STEPS_INDEX } from "../../src/server/elasticsearch";
-import { recreateIndexAndMigrate } from "../helpers";
-import { dspyStepsMapping } from "../schema";
-import { type Client as ElasticClient } from "@elastic/elasticsearch";
+import { TRACE_INDEX } from "../../src/server/elasticsearch";
+import { getCurrentWriteIndex } from "../helpers";
 
-export const migrate = async (migrationKey: string, client: ElasticClient) => {
-  await recreateIndexAndMigrate({
-    indexSpec: DSPY_STEPS_INDEX,
-    mapping: dspyStepsMapping as Record<string, MappingProperty>,
-    migrationKey,
+import { Client as ElasticClient } from "@elastic/elasticsearch";
+
+export const migrate = async (_migrationKey: string, client: ElasticClient) => {
+  const currentIndex = await getCurrentWriteIndex({
+    indexSpec: TRACE_INDEX,
     client,
+  });
+
+  await client.indices.putMapping({
+    index: currentIndex,
+    properties: {
+      metadata: {
+        properties: {
+          prompt_ids: { type: "keyword" },
+          prompt_version_ids: { type: "keyword" },
+        },
+      },
+    },
   });
 };
