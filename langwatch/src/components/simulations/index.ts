05f1505b70fc1d5eda8372b988a6616483b2d879
drewdrew <drewdrewthis@gmail.com>
2025-06-03 14:12:22 +0200

    Refactor into smaller components

--- langwatch/src/components/simulations/index.ts
+++ langwatch/src/components/simulations/index.ts
@@ -1,3 +1,5 @@
 export * from "./batch-card";
 export * from "./simulation-card";
 export * from "./simulations-grid";
+export * from "./CustomCopilotKitChat";
+export * from "./SimulationChatViewer";
