a95047f989049a1e34512e9e5180256aa7eb91d9
drewdrew <drewdrewthis@gmail.com>
2025-06-11 17:09:21 +0200

    Remove unused router methods

--- langwatch/src/server/api/routers/scenario.ts
+++ langwatch/src/server/api/routers/scenario.ts
@@ -34,7 +34,6 @@ export const scenarioRouter = createTRPCRouter({
       return data;
     }),
 
-  // OLD ROUTES
   // Get scenario run state
   getRunState: protectedProcedure
     .input(
@@ -53,95 +52,6 @@ export const scenarioRouter = createTRPCRouter({
         throw new Error("Scenario run not found");
       }
       return data;
-    }),
-
-  // Get scenario run history
-  getRunHistory: protectedProcedure
-    .input(
-      projectSchema.extend({
-        scenarioId: z.string(),
-      })
-    )
-    .use(checkUserPermissionForProject(TeamRoleGroup.SCENARIOS_VIEW))
-    .query(async ({ input, ctx }) => {
-      const scenarioRunnerService = new ScenarioRunnerService();
-      const { results } = await scenarioRunnerService.getScenarioResultsHistory(
-        {
-          projectId: input.projectId,
-          scenarioId: input.scenarioId,
-        }
-      );
-      return { results };
-    }),
-
-  // Get all scenario runs
-  getAllRuns: protectedProcedure
-    .input(projectSchema)
-    .use(checkUserPermissionForProject(TeamRoleGroup.SCENARIOS_VIEW))
-    .query(async ({ input, ctx }) => {
-      const scenarioRunnerService = new ScenarioRunnerService();
-      const ids = await scenarioRunnerService.getScenarioRunIds({
-        projectId: input.projectId,
-      });
-      return { ids };
-    }),
-
-  // Get all batch runs
-  getAllBatchRuns: protectedProcedure
-    .input(projectSchema)
-    .use(checkUserPermissionForProject(TeamRoleGroup.SCENARIOS_VIEW))
-    .query(async ({ input, ctx }) => {
-      const scenarioRunnerService = new ScenarioRunnerService();
-      const batches = await scenarioRunnerService.getAllBatchRunsForProject({
-        projectId: input.projectId,
-      });
-      return { batches };
-    }),
-
-  // Get scenario runs for a batch
-  getRunsForBatch: protectedProcedure
-    .input(
-      projectSchema.extend({
-        batchRunId: z.string(),
-      })
-    )
-    .use(checkUserPermissionForProject(TeamRoleGroup.SCENARIOS_VIEW))
-    .query(async ({ input, ctx }) => {
-      const scenarioRunnerService = new ScenarioRunnerService();
-      const ids = await scenarioRunnerService.getScenarioRunIdsForBatch({
-        projectId: input.projectId,
-        batchRunId: input.batchRunId,
-      });
-      return { ids };
-    }),
-
-  // Get scenario run data for a batch
-  getBatchRunData: protectedProcedure
-    .input(
-      projectSchema.extend({
-        batchRunId: z.string(),
-      })
-    )
-    .use(checkUserPermissionForProject(TeamRoleGroup.SCENARIOS_VIEW))
-    .query(async ({ input, ctx }) => {
-      const scenarioRunnerService = new ScenarioRunnerService();
-      const data = await scenarioRunnerService.getScenarioRunDataForBatch({
-        projectId: input.projectId,
-        batchRunId: input.batchRunId,
-      });
-      return { data: data ?? [] };
-    }),
-
-  // Get all events
-  getAllEvents: protectedProcedure
-    .input(projectSchema)
-    .use(checkUserPermissionForProject(TeamRoleGroup.SCENARIOS_VIEW))
-    .query(async ({ input, ctx }) => {
-      const scenarioRunnerService = new ScenarioRunnerService();
-      const events = await scenarioRunnerService.getAllRunEventsForProject({
-        projectId: input.projectId,
-      });
-      return { events };
     }),
 
   // Get scenario run data by scenario id
