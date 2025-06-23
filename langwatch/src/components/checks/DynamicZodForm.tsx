f0c60d8f9ffe6c9361bb3b25ac9389c2ba84c1eb
Richard Huth <richhuth@gmail.com>
2025-06-23 09:54:43 +0200

    Rabbit comment

--- langwatch/src/components/checks/DynamicZodForm.tsx
+++ langwatch/src/components/checks/DynamicZodForm.tsx
@@ -68,7 +68,7 @@ const ArrayField = <T extends EvaluatorTypes>({
     arraySchema.element instanceof z.ZodObject
       ? Object.fromEntries(
           Object.entries(arraySchema.element.shape).flatMap(([key, value]) => {
-            if (value instanceof z.ZodUnion) {
+            if (value instanceof z.ZodUnion && value.options.length > 0) {
               const defaultValue = value.options[0].value;
               return [[key, defaultValue]];
             }
