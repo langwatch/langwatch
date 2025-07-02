faba0739d5beb91e6abc79afab9caeb924963f6c
Alexander Forbes-Reed <git@alex.forbes.red>
2025-07-02 13:24:05 +0100

    use ms buckets

--- langwatch/src/server/metrics.ts
+++ langwatch/src/server/metrics.ts
@@ -15,7 +15,7 @@ register.removeSingleMetric("event_loop_lag_milliseconds");
 const eventLoopLag = new Histogram({
   name: "event_loop_lag_milliseconds",
   help: "Event loop lag in milliseconds",
-  buckets: [0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1, 2],
+  buckets: [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000]
 });
 setInterval(() => {
   const start = performance.now();
