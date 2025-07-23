626aafc3b877827b12cd8e9b52fe8c2d35d3e01f
Andrew Garde Joia <drewdrewthis@gmail.com>
2025-07-23 11:36:04 -0400

    feat: use query param for prompt id selection (#491)
    * Use query param for prompt id selection
    
    * refactor: extact custom hook and improve docs
    
    * Always open panel when row is clicked
    
    * remove commented code

--- langwatch/src/prompt-configs/PromptConfigPanel.tsx
+++ langwatch/src/prompt-configs/PromptConfigPanel.tsx
@@ -7,17 +7,16 @@ import {
   forwardRef,
   type ForwardedRef,
 } from "react";
-import { useDebouncedCallback } from "use-debounce";
 
+import { useDebouncedCallback } from "use-debounce";
 import { PanelHeader } from "./components/ui/PanelHeader";
 import { PromptConfigForm } from "./forms/prompt-config-form/PromptConfigForm";
-import { useInvokePrompt } from "./hooks/useInvokePrompt";
-import { usePromptConfigForm } from "./hooks/usePromptConfigForm";
-
 import {
   ExecutionInputPanel,
   type ExecuteData,
 } from "~/components/executable-panel/ExecutionInputPanel";
+import { usePromptConfigForm } from "./hooks/usePromptConfigForm";
+
 import { ExecutionOutputPanel } from "~/components/executable-panel/ExecutionOutputPanel";
 import {
   InputOutputExecutablePanel,
@@ -29,6 +28,7 @@ import {
   promptConfigFormValuesToOptimizationStudioNodeData,
 } from "~/prompt-configs/llmPromptConfigUtils";
 import { api } from "~/utils/api";
+import { useInvokePrompt } from "./hooks/useInvokePrompt";
 
 /**
  * Panel for configuring and testing LLM prompts
@@ -117,7 +117,6 @@ export const PromptConfigPanel = forwardRef(function PromptConfigPanel(
       setIsExpanded(false);
     } else {
       onClose();
-      setIsExpanded(true); // Reset to open state for next time
     }
   };
 
