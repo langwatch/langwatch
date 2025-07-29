eb5a9626093a39261018d7c8fcb6151db5eec631
Alexander Forbes-Reed <git@alex.forbes.red>
2025-07-29 16:03:11 +0200

    lint

--- typescript-sdk/src/observability/exporters/langwatch-exporter.ts
+++ typescript-sdk/src/observability/exporters/langwatch-exporter.ts
@@ -10,5 +10,5 @@ export function createLangWatchExporter(apiKey: string, endpointURL: string) {
       "X-LangWatch-SDK-Name": "langwatch-observability-sdk",
     },
     url: endpointURL,
-  })
+  });
 }
