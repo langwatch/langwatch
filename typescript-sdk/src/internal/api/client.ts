a59cce7801075d1463e1aa77887f63f969e0f19a
Alexander Forbes-Reed <git@alex.forbes.red>
2025-07-31 16:18:54 +0200

    generate openapi stuff

---
+++ typescript-sdk/src/internal/api/client.ts
@@ -0,0 +1,27 @@
+import openApiCreateClient from "openapi-fetch";
+import type { paths } from "../generated/openapi/api-client";
+import { z } from "zod";
+import { getApiKey, getEndpoint } from "../../client";
+
+const configSchema = z.object({
+  apiKey: z.string().min(1, "API key is required"),
+  endpoint: z.url(),
+});
+
+export function createLangWatchApiClient(apiKey?: string | undefined, endpoint?: string | undefined ) {
+  // This will error if the config is invalid
+  const config = configSchema.parse({
+    apiKey: apiKey ?? getApiKey(),
+    endpoint: endpoint ?? getEndpoint(),
+  });
+
+  return openApiCreateClient<paths>({
+    baseUrl: config.endpoint,
+    headers: {
+      "X-Auth-Token": config.apiKey,
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export type LangwatchApiClient = ReturnType<typeof createLangWatchApiClient>;
