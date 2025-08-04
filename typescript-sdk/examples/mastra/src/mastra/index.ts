84fe0d9e55819727a2a0d9b2339f3c0b65889459
Alexander Forbes-Reed <git@alex.forbes.red>
2025-08-04 10:18:09 +0200

    added mastra example

---
+++ typescript-sdk/examples/mastra/src/mastra/index.ts
@@ -0,0 +1,23 @@
+
+import { Mastra } from '@mastra/core/mastra';
+import { PinoLogger } from '@mastra/loggers';
+import { LibSQLStore } from '@mastra/libsql';
+import { weatherWorkflow } from './workflows/weather-workflow.js';
+import { weatherAgent } from './agents/weather-agent.js';
+
+export const mastra = new Mastra({
+  workflows: { weatherWorkflow },
+  agents: { weatherAgent },
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
+  }),
+  logger: new PinoLogger({
+    name: 'Mastra',
+    level: 'info',
+  }),
+  telemetry: {
+    enabled: true,
+    serviceName: "mastra-and-vercel-ai-sdk-example",
+  },
+});
