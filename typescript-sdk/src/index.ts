2d5d818157ae8d16d350745eefd704868b60b8b3
Alexander Forbes-Reed <git@alex.forbes.red>
2025-08-05 17:42:01 +0200

    remove old code

--- typescript-sdk/src/index.ts
+++ typescript-sdk/src/index.ts
@@ -4,11 +4,3 @@ export {
   type SpanProcessingExcludeRule,
 } from "./observability/processors";
 export { LangWatchExporter } from "./observability/exporters";
-
-export { recordEvaluation, runEvaluation } from "./evaluation";
-
-export { getPrompt, getPromptVersion } from "./prompt";
-
-import { PromptFacade } from "./prompt/facade";
-
-export const prompts = PromptFacade.getInstance();
