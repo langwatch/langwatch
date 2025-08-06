69dae4c479080e91a20269f08b544360376468be
drewdrew <drewdrewthis@gmail.com>
2025-08-06 19:00:19 +0200

    use mts for vite

--- typescript-sdk/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    testTimeout: 30_000,
-    hookTimeout: 30_000,
-    setupFiles: ["dotenv/config"],
-  },
-});
