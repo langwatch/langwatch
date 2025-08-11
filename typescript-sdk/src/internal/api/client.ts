14c84dc544a98ffe58227d2102dfd1be658f3c3d
Alexander Forbes-Reed <git@alex.forbes.red>
2025-08-11 16:14:29 +0200

    review comment

--- typescript-sdk/src/internal/api/client.ts
+++ typescript-sdk/src/internal/api/client.ts
@@ -1,19 +1,23 @@
 import openApiCreateClient from "openapi-fetch";
 import type { paths } from "../generated/openapi/api-client";
 import { version } from "../../../package.json";
-import { LANGWATCH_SDK_LANGUAGE, LANGWATCH_SDK_NAME_OBSERVABILITY, LANGWATCH_SDK_RUNTIME, LANGWATCH_SDK_VERSION } from "../constants";
+import {
+  LANGWATCH_SDK_LANGUAGE,
+  LANGWATCH_SDK_NAME_OBSERVABILITY,
+  LANGWATCH_SDK_RUNTIME,
+  LANGWATCH_SDK_VERSION,
+} from "../constants";
 
 export type LangwatchApiClient = ReturnType<typeof openApiCreateClient<paths>>;
 
 export function createLangWatchApiClient(
-  apiKey?: string  ,
-  endpoint?: string  ,
+  apiKey?: string,
+  endpoint?: string
 ): LangwatchApiClient {
   return openApiCreateClient<paths>({
     baseUrl: endpoint,
     headers: {
-      "authorization": `Bearer ${apiKey}`,
-      "x-auth-token": apiKey,
+      ...(apiKey ? { authorization: `Bearer ${apiKey}`, 'x-auth-token': apiKey } : {}),
       "content-type": "application/json",
       "user-agent": `langwatch-sdk-node/${version}`,
       "x-langwatch-sdk-name": LANGWATCH_SDK_NAME_OBSERVABILITY,
