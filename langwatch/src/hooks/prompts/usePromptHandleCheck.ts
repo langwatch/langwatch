3a251a0c29a164e51d4fbaad91a1a410cbdec521
drewdrew <drewdrewthis@gmail.com>
2025-09-24 18:15:43 +0200

    update prompt provider

--- langwatch/src/hooks/prompts/usePromptHandleCheck.ts
+++ langwatch/src/hooks/prompts/usePromptHandleCheck.ts
@@ -1,4 +1,5 @@
 import { type PromptScope } from "@prisma/client";
+
 import { useOrganizationTeamProject } from "../useOrganizationTeamProject";
 
 import { api } from "~/utils/api";
@@ -10,12 +11,11 @@ export const usePromptHandleCheck = () => {
   const checkHandleUniqueness = async (params: {
     handle: string;
     scope: PromptScope;
-    excludeId?: string;
   }) => {
     const isValid = await trpc.llmConfigs.checkHandleUniqueness.fetch({
-      ...params,
       projectId: project?.id ?? "",
       scope: params.scope,
+      handle: params.handle,
     });
 
     return isValid;
