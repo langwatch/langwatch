cd281871714cb859b1ecbf7687750219b51247aa
Alexander Forbes-Reed <git@alex.forbes.red>
2025-10-08 17:41:37 +0200

    refactor onboarding container to include title

--- langwatch/src/features/onboarding/components/OnboardingContainer.tsx
+++ langwatch/src/features/onboarding/components/OnboardingContainer.tsx
@@ -1,23 +1,47 @@
-import { Box, Center, Container, SkeletonText } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Container,
+  SkeletonText,
+  VStack,
+  Text,
+} from "@chakra-ui/react";
 import { FullLogo } from "~/components/icons/FullLogo";
 
 interface OnboardingContainerProps extends React.PropsWithChildren {
   loading?: boolean;
+  title: string;
+  subTitle?: string;
 }
 
-export const OrganizationOnboardingContainer: React.FC<OnboardingContainerProps> = ({ children, loading }) => {
+export const OrganizationOnboardingContainer: React.FC<
+  OnboardingContainerProps
+> = ({ children, loading, title, subTitle }) => {
   return (
-    <Box w="dvw" h="dvh" pt={"10"} background="bg.subtle">
+    <Box w="dvw" h="dvh" pt={"12vh"} background="bg.subtle">
       <Center>
         <FullLogo width={175} />
       </Center>
 
       <Container mt={"10"} maxW={"700px"}>
-        {loading && (
-          <SkeletonText variant={"pulse"} noOfLines={2} loading={loading} />
-        )}
-        {!loading && children}
+        <VStack gap={4} align="stretch">
+          <VStack gap={0} align="start">
+            <Text textStyle={"2xl"} fontWeight={"bold"} color={"WindowText"}>
+              {title}
+            </Text>
+            {subTitle && (
+              <Text textStyle={"md"} color={"WindowText"}>
+                {subTitle}
+              </Text>
+            )}
+          </VStack>
+
+          {loading && (
+            <SkeletonText variant={"pulse"} noOfLines={2} loading={loading} />
+          )}
+          {!loading && children}
+        </VStack>
       </Container>
     </Box>
   );
-}
+};
