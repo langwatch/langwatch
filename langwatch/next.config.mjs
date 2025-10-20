d22fbd0acee8fdb8e95a1e13aa113a259fa5a9c2
Alexander Forbes-Reed <git@alex.forbes.red>
2025-10-20 16:02:42 +0200

    add raw-loader, update turbopack config

--- langwatch/next.config.mjs
+++ langwatch/next.config.mjs
@@ -10,7 +10,7 @@ process.env.SENTRY_IGNORE_API_RESOLUTION_ERROR = "1";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const aliasPath =
-  process.env.DEPENDENCY_INJECTION_DIR ?? path.join("src", "injection");
+    process.env.DEPENDENCY_INJECTION_DIR ?? path.join("src", "injection");
 
 const cspHeader = `
     default-src 'self';
@@ -30,7 +30,7 @@ const cspHeader = `
 `;
 
 const existingNodeModules = new Set(
-  fs.readdirSync(path.join(__dirname, "node_modules"))
+    fs.readdirSync(path.join(__dirname, "node_modules"))
 );
 
 /**
@@ -41,146 +41,158 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  logging: false,
-  distDir: process.env.NEXTJS_DIST_DIR ?? ".next",
+    reactStrictMode: true,
+    logging: false,
+    distDir: process.env.NEXTJS_DIST_DIR ?? ".next",
 
-  typescript: {
-    // Typechecking here is slow, and is now handled by a dedicated CI job using tsgo!
-    ignoreBuildErrors: true,
-  },
-
-  experimental: {
-    scrollRestoration: true,
-    turbo: {
-      resolveAlias: {
-        "@injected-dependencies.client": path.join(
-          aliasPath,
-          "injection.client.ts"
-        ),
-        "@injected-dependencies.server": path.join(
-          aliasPath,
-          "injection.server.ts"
-        ),
-
-        // read all folders from ./saas-src/node_modules and create a map like the above
-        ...(fs.existsSync(path.join(__dirname, "saas-src", "node_modules"))
-          ? Object.fromEntries(
-              fs
-                .readdirSync(path.join(__dirname, "saas-src", "node_modules"))
-                .filter((key) => !existingNodeModules.has(key))
-                .flatMap((key) => [
-                  [key, `./saas-src/node_modules/${key}`],
-                  [`${key}/*`, `./saas-src/node_modules/${key}/*`],
-                ])
-            )
-          : {}),
-      },
+    typescript: {
+        // Typechecking here is slow, and is now handled by a dedicated CI job using tsgo!
+        ignoreBuildErrors: true,
     },
-    optimizePackageImports: [
-      "@chakra-ui/react",
-      "react-feather",
-      "@zag-js",
-      "@mui",
-    ],
-  },
+    turbopack: {
+        rules: {
+            "*.snippet.ts": { loaders: ["raw-loader"], as: "*.js" },
+            "*.snippet.go": { loaders: ["raw-loader"], as: "*.js" },
+            "*.snippet.sh": { loaders: ["raw-loader"], as: "*.js" },
+            "*.snippet.py": { loaders: ["raw-loader"], as: "*.js" },
+        },
+        resolveAlias: {
+            "@injected-dependencies.client": path.join(
+                aliasPath,
+                "injection.client.ts"
+            ),
+            "@injected-dependencies.server": path.join(
+                aliasPath,
+                "injection.server.ts"
+            ),
 
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Referrer-Policy",
-            value: "no-referrer",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=31536000; includeSubDomains",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
+            // read all folders from ./saas-src/node_modules and create a map like the above
+            ...(fs.existsSync(path.join(__dirname, "saas-src", "node_modules"))
+                ? Object.fromEntries(
+                    fs
+                        .readdirSync(path.join(__dirname, "saas-src", "node_modules"))
+                        .filter((key) => !existingNodeModules.has(key))
+                        .flatMap((key) => [
+                            [key, `./saas-src/node_modules/${key}`],
+                            [`${key}/*`, `./saas-src/node_modules/${key}/*`],
+                        ]),
+                )
+                : {}),
+        },
+    },
+
+    experimental: {
+        scrollRestoration: true,
+        optimizePackageImports: [
+            "@chakra-ui/react",
+            "react-feather",
+            "@zag-js",
+            "@mui",
         ],
-      },
-    ];
-  },
+    },
 
-  webpack: (config) => {
-    config.resolve.alias["@injected-dependencies.client"] = path.join(
-      aliasPath,
-      "injection.client.ts"
-    );
-    config.resolve.alias["@injected-dependencies.server"] = path.join(
-      aliasPath,
-      "injection.server.ts"
-    );
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Referrer-Policy",
+                        value: "no-referrer",
+                    },
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=31536000; includeSubDomains",
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: cspHeader.replace(/\n/g, ""),
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
+                ],
+            },
+        ];
+    },
 
-    // Ensures that only a single version of those are ever loaded
-    // eslint-disable-next-line @typescript-eslint/dot-notation
-    config.resolve.alias["react"] = `${__dirname}/node_modules/react`;
-    config.resolve.alias["react-dom"] = `${__dirname}/node_modules/react-dom`;
-    // eslint-disable-next-line @typescript-eslint/dot-notation
-    config.resolve.alias["next"] = `${__dirname}/node_modules/next`;
-    config.resolve.alias["next-auth"] = `${__dirname}/node_modules/next-auth`;
-    // eslint-disable-next-line @typescript-eslint/dot-notation
-    config.resolve.alias["zod"] = `${__dirname}/node_modules/zod`;
+    webpack: (config) => {
+        config.resolve.alias["@injected-dependencies.client"] = path.join(
+            aliasPath,
+            "injection.client.ts"
+        );
+        config.resolve.alias["@injected-dependencies.server"] = path.join(
+            aliasPath,
+            "injection.server.ts"
+        );
 
-    // Add fallback for pino logger requirements
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      worker_threads: false,
-      fs: false,
-    };
+        // Ensures that only a single version of those are ever loaded
+        // eslint-disable-next-line @typescript-eslint/dot-notation
+        config.resolve.alias["react"] = `${__dirname}/node_modules/react`;
+        config.resolve.alias["react-dom"] = `${__dirname}/node_modules/react-dom`;
+        // eslint-disable-next-line @typescript-eslint/dot-notation
+        config.resolve.alias["next"] = `${__dirname}/node_modules/next`;
+        config.resolve.alias["next-auth"] = `${__dirname}/node_modules/next-auth`;
+        // eslint-disable-next-line @typescript-eslint/dot-notation
+        config.resolve.alias["zod"] = `${__dirname}/node_modules/zod`;
 
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      use: [
-        {
-          loader: "string-replace-loader",
-          options: {
-            search: /@langwatch-oss\/node_modules\//g,
-            replace: "",
-            flags: "g",
-          },
-        },
-        {
-          loader: "string-replace-loader",
-          options: {
-            search: /@langwatch-oss\/src\//g,
-            replace: "~/",
-            flags: "g",
-          },
-        },
-      ],
-    });
+        // Add fallback for pino logger requirements
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            worker_threads: false,
+            fs: false,
+        };
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return config;
-  },
+        config.module.rules.push({
+            test: /\.(js|jsx|ts|tsx)$/,
+            use: [
+                {
+                    loader: "string-replace-loader",
+                    options: {
+                        search: /@langwatch-oss\/node_modules\//g,
+                        replace: "",
+                        flags: "g",
+                    },
+                },
+                {
+                    loader: "string-replace-loader",
+                    options: {
+                        search: /@langwatch-oss\/src\//g,
+                        replace: "~/",
+                        flags: "g",
+                    },
+                },
+            ],
+        });
+
+        // Support importing files with `?snippet` to get source content for IDE-highlighted snippets
+        config.module.rules.push({ resourceQuery: /snippet/, type: "asset/source" });
+
+        // Treat any *.snippet.* files as source assets to avoid resolution inside snippets
+        config.module.rules.push({ test: /\.snippet\.(txt|ts|tsx|js|go|sh|py)$/i, type: "asset/source" });
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return config;
+    },
 };
 
 export default bundleAnalyser({ enabled: process.env.ANALYZE === "true" })(
-  withSentryConfig(config, {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
+    withSentryConfig(config, {
+        // For all available options, see:
+        // https://github.com/getsentry/sentry-webpack-plugin#options
 
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "langwatch",
-    project: "langwatch",
+        // Suppresses source map uploading logs during build
+        silent: true,
+        org: "langwatch",
+        project: "langwatch",
 
-    widenClientFileUpload: true,
-    tunnelRoute: "/monitoring",
-    sourcemaps: {
-      disable: false,
-      deleteSourcemapsAfterUpload: true,
-    },
-    disableLogger: true,
-  })
+        widenClientFileUpload: true,
+        tunnelRoute: "/monitoring",
+        sourcemaps: {
+            disable: false,
+            deleteSourcemapsAfterUpload: true,
+        },
+        disableLogger: true,
+    })
 );
