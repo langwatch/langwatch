58d3725a8953cb57bbacb354d27e6c134d3a3aec
drewdrew <drewdrewthis@gmail.com>
2025-10-23 11:59:32 +0200

    remove tests

--- langwatch/src/prompt-configs/prompt-studio/components/sidebar/PromptStudioSidebar.tsx
+++ langwatch/src/prompt-configs/prompt-studio/components/sidebar/PromptStudioSidebar.tsx
@@ -1,10 +1,7 @@
 import { Sidebar } from "./ui/Sidebar";
 import { PublishedPromptsList } from "./PublishedPromptsList";
-import { DraftPromptsList } from "./DraftPromptsList";
 import { Text } from "@chakra-ui/react";
 import { AddPromptButton } from "./AddPromptButton";
-import { SessionSnapshotsList } from "./SessionSnapshotsList";
-import { ConversationHistoryList } from "./ConversationHistoryList";
 
 /**
  * The Prompt Studio sidebar component.
@@ -19,15 +16,7 @@ export function PromptStudioSidebar() {
           <AddPromptButton />
         </Sidebar.SectionHeader>
         <PublishedPromptsList />
-        {/* <DraftPromptsList /> */}
       </Sidebar.Section>
-      {/* <Sidebar.Section>
-        <Sidebar.SectionHeader>
-          <Text>Sessions</Text>
-        </Sidebar.SectionHeader>
-        <SessionSnapshotsList />
-        <ConversationHistoryList />
-      </Sidebar.Section> */}
     </Sidebar.Root>
   );
 }
--- langwatch/src/prompt-configs/prompt-studio/components/sidebar/SessionSnapshotsList.tsx
+++
@@ -1,9 +0,0 @@
-import { Sidebar } from "./ui/Sidebar";
-
-export function SessionSnapshotsList() {
-  return (
-    <Sidebar.List title="Saved" collapsible>
-      <Sidebar.Item variant="empty">No saved sessions</Sidebar.Item>
-    </Sidebar.List>
-  );
-}
