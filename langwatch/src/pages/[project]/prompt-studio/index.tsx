cba19cc5f6a2d5db5e7d0afa5493479096304248
drewdrew <drewdrewthis@gmail.com>
2025-10-27 11:34:49 +0100

    styling for chat

--- langwatch/src/pages/[project]/prompt-studio/index.tsx
+++ langwatch/src/pages/[project]/prompt-studio/index.tsx
@@ -1,5 +1,6 @@
 import { PromptStudioLayout } from "~/prompt-configs/prompt-studio/components/PromptStudioLayout";
 import "@copilotkit/react-ui/styles.css";
+import "./styles.css";
 
 export default function PromptStudioPage() {
   return <PromptStudioLayout />;
