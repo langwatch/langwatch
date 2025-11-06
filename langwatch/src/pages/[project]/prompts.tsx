16fb9117459a9f229fa9cf7c52c2e707db173e9b
drewdrew <drewdrewthis@gmail.com>
2025-11-06 11:56:01 +0100

    fix: permission layout

--- langwatch/src/pages/[project]/prompts.tsx
+++ langwatch/src/pages/[project]/prompts.tsx
@@ -1,9 +1,12 @@
 import { PromptStudioLayout } from "~/prompt-configs/prompt-studio/components/PromptStudioLayout";
 import { withPermissionGuard } from "~/components/WithPermissionGuard";
 import "@copilotkit/react-ui/styles.css";
+import { DashboardLayout } from "~/components/DashboardLayout";
 
 function PromptStudioPage() {
   return <PromptStudioLayout />;
 }
 
-export default withPermissionGuard("prompts:view")(PromptStudioPage);
+export default withPermissionGuard("prompts:view", {
+  layoutComponent: DashboardLayout,
+})(PromptStudioPage);
