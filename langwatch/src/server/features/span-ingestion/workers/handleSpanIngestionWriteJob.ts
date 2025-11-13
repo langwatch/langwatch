95f282549588e20b128037dbb3b8457bf59fd691
Alexander Forbes-Reed <git@alex.forbes.red>
2025-11-13 18:54:07 +0100

    linked consumer to worker

--- langwatch/src/server/features/span-ingestion/workers/handleSpanIngestionWriteJob.ts
+++ langwatch/src/server/features/span-ingestion/workers/handleSpanIngestionWriteJob.ts
@@ -1,26 +1,9 @@
 import type { Job } from "bullmq";
-import { createLogger } from "../../../../utils/logger";
 import type { SpanIngestionWriteJob } from "../types/";
-
-const logger = createLogger("langwatch:features:span-ingestion:workers:handleSpanIngestionWriteJob");
+import { spanIngestionWriteConsumerClickHouse } from "../consumers";
 
 export async function handleSpanIngestionWriteJob(
   job: Job<SpanIngestionWriteJob>,
 ): Promise<void> {
-  const { tenantId, spanData, collectedAtUnixMs } = job.data;
-
-  logger.info({
-    tenantId,
-    spanId: spanData.spanId,
-    traceId: spanData.traceId,
-    collectedAtUnixMs,
-  }, "handleSpanIngestionWriteJob");
-
-  // TODO: Implement the actual span ingestion logic using spanData (the DTO)
-  // spanData now contains all the JSON-serializable span information:
-  // - spanData.traceId, spanData.spanId, spanData.traceFlags, etc.
-  // - spanData.attributes, spanData.events, spanData.status, etc.
-  // - spanData.resourceAttributes, spanData.instrumentationScope, etc.
-
-  throw new Error("not implemented yet");
+  await spanIngestionWriteConsumerClickHouse.consume(job.data);
 }
