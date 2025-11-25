6f8b31690985fd0ffbb7be61816fb8117a97d0ce
Richard Huth <richhuth@gmail.com>
2025-11-25 15:08:25 +0100

    cleanup migration

--- langwatch/prisma/migrations/20251121112408_add_copied_from_workflow_id/migration.sql
+++ langwatch/prisma/migrations/20251121112408_add_copied_from_workflow_id/migration.sql
@@ -4,3 +4,9 @@ ALTER TABLE "Workflow" ADD COLUMN     "copiedFromWorkflowId" TEXT;
 
 -- CreateIndex
 CREATE INDEX "Workflow_copiedFromWorkflowId_idx" ON "Workflow"("copiedFromWorkflowId");
+
+-- AlterTable
+ALTER TABLE "LlmPromptConfig" ADD COLUMN     "copiedFromPromptId" TEXT;
+
+-- CreateIndex
+CREATE INDEX "LlmPromptConfig_copiedFromPromptId_idx" ON "LlmPromptConfig"("copiedFromPromptId");
