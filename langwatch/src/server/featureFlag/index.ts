c93857660fdab0108926b957c626e49586d53a05
Alexander Forbes-Reed <git@alex.forbes.red>
2025-12-19 01:41:29 +0100

    new feature flag service + swanky caching

---
+++ langwatch/src/server/featureFlag/index.ts
@@ -0,0 +1,4 @@
+export { FeatureFlagService, featureFlagService } from "./featureFlag.service";
+export { PostHogFeatureFlagService } from "./posthogFeatureFlag.service";
+export { MemoryFeatureFlagService } from "./memoryFeatureFlag.service";
+export type { FeatureFlagServiceInterface } from "./types";
