1a2b87426235c70e5416d38f0beab63eeb759684
Alexander Forbes-Reed <git@alex.forbes.red>
2026-01-12 14:22:50 +0100

    fix migration

--- langwatch/prisma/migrations/20260111125259_improve_event_sourcing_and_clickhouse_feature_flags/migration.sql
+++ langwatch/prisma/migrations/20260111125259_improve_event_sourcing_and_clickhouse_feature_flags/migration.sql
@@ -4,9 +4,6 @@
   - You are about to drop the column `featureClickHouse` on the `Project` table. All the data in the column will be lost.
 
 */
--- DropIndex
-DROP INDEX "CustomGraph_id_key";
-
 -- AlterTable
 ALTER TABLE "Project" DROP COLUMN "featureClickHouse",
 ADD COLUMN     "featureClickHouseDataSourceEvaluations" BOOLEAN NOT NULL DEFAULT false,
