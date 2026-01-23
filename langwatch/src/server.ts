10ec064de252cf15604d9848ecfab9f12b388176
Alexander Forbes-Reed <github@alex.forbes.red>
2026-01-23 15:41:05 +0100

    fix: set correct ksuid environment in worker (#1173)
    set correct environment for ksuid

--- langwatch/src/server.ts
+++ langwatch/src/server.ts
@@ -2,7 +2,7 @@ import { setEnvironment } from "@langwatch/ksuid";
 import dotenv from "dotenv";
 
 dotenv.config();
-setEnvironment(process.env.NODE_ENV ?? "dev");
+setEnvironment(process.env.ENVIRONMENT ?? "local");
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { startApp } = require("./start.js");
