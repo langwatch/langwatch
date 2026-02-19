b23b201f9179c9fc6d770d971438bce8c7b69239
Alexander Forbes-Reed <git@alex.forbes.red>
2026-02-19 13:23:05 +0100

    renames

--- langwatch/packages/deja-view/src/discovery/eventHandlers.types.ts
+++ langwatch/packages/deja-view/src/discovery/eventHandlers.types.ts
@@ -1,5 +1,5 @@
-import type { MapProjectionDefinition } from "../../../../src/server/event-sourcing/library/projections/mapProjection.types";
-import type { Event } from "../../../../src/server/event-sourcing/library/domain/types";
+import type { MapProjectionDefinition } from "../../../../src/server/event-sourcing/projections/mapProjection.types";
+import type { Event } from "../../../../src/server/event-sourcing/domain/types";
 
 /**
  * Metadata for a map projection (formerly event handler) discovered from a pipeline.
