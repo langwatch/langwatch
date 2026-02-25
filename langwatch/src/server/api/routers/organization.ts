d503d7960b29626b3713ff5152d7c3f2f1ce35a9
Sergio Esteban <sergio.esteban.ce@gmail.com>
2026-02-25 15:40:07 +0100

    fix: change default org pricing model from TIERED to SEAT_EVENT
    New organizations created during onboarding now default to the
    SEAT_EVENT pricing model instead of TIERED.

--- langwatch/src/server/api/routers/organization.ts
+++ langwatch/src/server/api/routers/organization.ts
@@ -290,7 +290,7 @@ export const organizationRouter = createTRPCRouter({
               slug: orgSlug,
               phoneNumber: input.phoneNumber,
               signupData: input.signUpData,
-              pricingModel: PricingModel.TIERED,
+              pricingModel: PricingModel.SEAT_EVENT,
             },
           });
 
