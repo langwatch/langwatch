5f2de9aa7de230ed9e5d0ff1e9a73cdbbb266796
Sergio Esteban <sergio.esteban.ce@gmail.com>
2026-02-25 18:35:25 +0100

    fix(contact): update contact linkt from mail to hubspot link

--- langwatch/src/components/plans/constants.ts
+++ langwatch/src/components/plans/constants.ts
@@ -1,2 +1,2 @@
 export const CONTACT_SALES_URL =
-  "https://meetings-eu1.hubspot.com/pedro-silva2/langwatch-enterprise";
+  "https://meetings-eu1.hubspot.com/manouk-draisma?uuid=3c29cf0c-03e5-4a53-81fd-94abb0b66cfd";
