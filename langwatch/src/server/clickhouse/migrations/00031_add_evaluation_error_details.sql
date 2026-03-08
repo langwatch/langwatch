0cd5dd699f904e3020fb70041afaaa46ba8ae6b6
Alexander Forbes-Reed <git@alex.forbes.red>
2026-03-09 00:22:07 +0100

    fix migration format

--- langwatch/src/server/clickhouse/migrations/00031_add_evaluation_error_details.sql
+++ langwatch/src/server/clickhouse/migrations/00031_add_evaluation_error_details.sql
@@ -1,2 +1,15 @@
--- Add ErrorDetails column to evaluation_runs for storing extended error context (e.g. stack traces)
-ALTER TABLE evaluation_runs ADD COLUMN IF NOT EXISTS ErrorDetails Nullable(String) AFTER Error;
+-- +goose Up
+-- +goose ENVSUB ON
+-- +goose StatementBegin
+ALTER TABLE ${CLICKHOUSE_DATABASE}.evaluation_runs
+    ADD COLUMN IF NOT EXISTS ErrorDetails Nullable(String) AFTER Error;
+-- +goose StatementEnd
+-- +goose ENVSUB OFF
+
+-- +goose Down
+-- +goose ENVSUB ON
+-- +goose StatementBegin
+-- ALTER TABLE ${CLICKHOUSE_DATABASE}.evaluation_runs
+--    DROP COLUMN IF EXISTS ErrorDetails;
+-- +goose StatementEnd
+-- +goose ENVSUB OFF
