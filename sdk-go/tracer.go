55986044c00be55343481e8fb8343e9b16333d60
seungkon.kang <seungkon.kang@trillionlabs.co>
2026-04-06 17:10:14 +0900

    fix(go-sdk): middleware ignores tracerProvider option

--- sdk-go/tracer.go
+++ sdk-go/tracer.go
@@ -17,6 +17,17 @@ func Tracer(name string, options ...trace.TracerOption) *LangWatchTracer {
 	}
 }
 
+// TracerFromProvider creates a LangWatchTracer using the given TracerProvider
+// instead of the global one. If provider is nil, it falls back to the global TracerProvider.
+func TracerFromProvider(provider trace.TracerProvider, name string, options ...trace.TracerOption) *LangWatchTracer {
+	if provider == nil {
+		provider = otel.GetTracerProvider()
+	}
+	return &LangWatchTracer{
+		tracer: provider.Tracer(name, options...),
+	}
+}
+
 func (t *LangWatchTracer) Start(ctx context.Context, name string, opts ...trace.SpanStartOption) (context.Context, *Span) {
 	ctx, span := t.tracer.Start(ctx, name, opts...)
 	return ctx, &Span{span}
