54eb77dd93eab730a8d2b941c5c48b6f667c0c98
Alexander Forbes-Reed <git@alex.forbes.red>
2026-04-15 15:44:30 +0200

    fix: use theme-aware JsonViewer in Foundry JSON view
    Replace raw <pre> block with the existing JsonViewer component which
    handles syntax coloring and theme awareness. Fix extra top padding.

--- langwatch/src/components/ops/foundry/views/JsonView.tsx
+++ langwatch/src/components/ops/foundry/views/JsonView.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Flex } from "@chakra-ui/react";
 import { Copy } from "lucide-react";
 import { useState } from "react";
 import { useTraceStore } from "../traceStore";
+import { JsonViewer } from "~/components/ops/JsonViewer";
 
 export function JsonView() {
   const trace = useTraceStore((s) => s.trace);
@@ -14,31 +15,14 @@ export function JsonView() {
   }
 
   return (
-    <Box p={4} minW={0} overflow="hidden">
-      <Flex justify="flex-end" mb={2}>
+    <Box minW={0} overflow="hidden">
+      <Flex justify="flex-end" px={4} pt={3} pb={1}>
         <Button size="xs" variant="outline" onClick={copyJson}>
           <Copy size={12} />
           {copied ? "Copied!" : "Copy JSON"}
         </Button>
       </Flex>
-      <Box
-        as="pre"
-        maxH="500px"
-        overflow="auto"
-        rounded="lg"
-        border="1px solid"
-        borderColor="border"
-        bg="bg.subtle"
-        p={3}
-        fontFamily="mono"
-        fontSize="xs"
-        color="fg.default"
-        whiteSpace="pre-wrap"
-        wordBreak="break-all"
-        overflowX="hidden"
-      >
-        {JSON.stringify(trace, null, 2)}
-      </Box>
+      <JsonViewer data={trace} maxHeight="calc(100vh - 180px)" />
     </Box>
   );
 }
