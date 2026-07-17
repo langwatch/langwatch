bbe5edff8b53bb3acbf537fb2a2f1bf9bf119420
Alexander Forbes-Reed <git@alex.forbes.red>
2026-07-17 19:19:36 +0200

    refactor(topic-clustering): clusterTopicsForProject returns a page outcome instead of self-enqueueing

--- langwatch/src/tasks/runTopicClustering.ts
+++ langwatch/src/tasks/runTopicClustering.ts
@@ -1,5 +1,16 @@
-import { clusterTopicsForProject } from "../server/topicClustering/topicClustering";
+import {
+  clusterTopicsForProject,
+  type ClusteringPageOutcome,
+} from "../server/topicClustering/topicClustering";
 
 export default async function execute(projectId: string) {
-  await clusterTopicsForProject(projectId, undefined, true);
+  let searchAfter: ClusteringPageOutcome["nextSearchAfter"];
+  do {
+    const outcome = await clusterTopicsForProject(projectId, searchAfter);
+    console.log(
+      `mode=${outcome.mode} traces=${outcome.tracesProcessed}` +
+        (outcome.skippedReason ? ` skipped=${outcome.skippedReason}` : ""),
+    );
+    searchAfter = outcome.nextSearchAfter;
+  } while (searchAfter);
 }
