0dde2f2dd7d7b2fef0388698d2c4541c17b2a704
Alexander Forbes-Reed <git@alex.forbes.red>
2026-09-07 02:08:15 +0200

    The langy contract and user server indexes name their files on disk, and the user index names the sign-in credential adapter that exists

--- packages/features/user/server/src/index.ts
+++ packages/features/user/server/src/index.ts
@@ -1,44 +1,44 @@
-export { PostgresUserAdapter } from "./adapters/postgres.user.adapter";
+export { PostgresUserAdapter } from "./adapters/postgres.user.adapter.ts";
 export {
   PostgresUserCredentialAdapter,
   type PostgresUserCredentialAdapterOptions,
   type UserCredentialDatabase,
-} from "./adapters/postgres.user-credential.adapter";
+} from "./adapters/postgres.user-signin-credential.adapter.ts";
 export {
   PersonalProjectKeyRequiredError,
   PersonalUsageKeyMismatchError,
   UserApp,
   type UserAppDependencies,
-} from "./app/user.app";
-export { UserAvatarStoragePort, UserPasswordHasherPort } from "./ports/user.port";
+} from "./app/user.app.ts";
+export { UserAvatarStoragePort, UserPasswordHasherPort } from "./ports/user.port.ts";
 export {
   UserCredentialService,
   type UserPasswordRotationOutcome,
-} from "./services/user-credential.service";
-export { UserService } from "./services/user.service";
+} from "./services/user-signin-credential.service.ts";
+export { UserService } from "./services/user.service.ts";
 export {
   runGdprUserDataErase,
   UserDataEraseTask,
   type GdprUserDataEraseOutcome,
-} from "./tasks/user-data-erase.task";
-export { PostgresUserDataEraseAdapter } from "./adapters/postgres.user-data-erase.adapter";
+} from "./tasks/user-data-erase.task.ts";
+export { PostgresUserDataEraseAdapter } from "./adapters/postgres.user-data-erase.adapter.ts";
 export {
   IdentityTrpcApi,
   type IdentityTrpcContext,
   type IdentityTrpcPorts,
-} from "./transport/api-trpc/identity.api";
+} from "./transport/api-trpc/identity.api.ts";
 export {
   UserTrpcApi,
   type Auth0PasswordChangeOutcome,
   type UnlinkAccountOutcome,
   type UserTrpcContext,
   type UserTrpcPorts,
-} from "./transport/api-trpc/user.api";
-export { createMeRestApp, type MePersonalUsageReader } from "./transport/api-rest/me.api";
+} from "./transport/api-trpc/user.api.ts";
+export { createMeRestApp, type MePersonalUsageReader } from "./transport/api-rest/me.api.ts";
 export {
   createUserAvatarRestApp,
   type UserAvatarDualAuthVariables,
   type UserAvatarObjectReader,
   type UserAvatarRateLimiter,
   type UserAvatarStoredObjectRead,
-} from "./transport/api-rest/user-avatar.api";
+} from "./transport/api-rest/user-avatar.api.ts";
