de8fc01903d6f11aecc3a040d1c201b60909e2ed
Alexander Forbes-Reed <git@alex.forbes.red>
2026-09-07 11:42:21 +0200

    Defer the GitHub installer until its API prerequisites are reviewed

--- packages/features/github/server/src/github.server.ts
+++
@@ -1,6 +0,0 @@
-import { defineFeature } from "@langwatch/runtime-composition";
-import { GithubApp, type GithubInfrastructure } from "./app/github.app.ts";
-
-export type { GithubInfrastructure } from "./app/github.app.ts";
-
-export const githubServer = defineFeature("github").withApp(GithubApp).build();
